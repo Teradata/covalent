84420d29cf21486bae666d9ebd37a3267c41f471
Kyle Ledbetter <kyle.ledbetter@teradata.com>
2016-07-14 12:05:32 -0500

    feature(docs): add utility styles docs

---
+++ src/app/components/style-guide/utility-styles/utility-styles.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+
+import { UtilityStylesComponent } from './utility-styles.component';
+
+describe('Component: UtilityStyles', () => {
+  it('should create an instance', () => {
+    let component = new UtilityStylesComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/style-guide/utility-styles/utility-styles.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MdIcon } from '@angular2-material/icon';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { TdHighlightComponent } from '../../../../platform/highlight';
+
+@Component({
+  directives: [
+    MD_CARD_DIRECTIVES,
+    MdIcon,
+    TdHighlightComponent,
+    MD_BUTTON_DIRECTIVES,
+  ],
+  moduleId: module.id,
+  selector: 'td-utility-styles',
+  templateUrl: 'utility-styles.component.html',
+  styleUrls: ['utility-styles.component.css']
+})
+export class UtilityStylesComponent {
+
+}
