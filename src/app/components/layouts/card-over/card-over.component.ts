97127541b4a51d65c6bf8f5ee8f859f6a87f747a
Kyle Ledbetter <kyle@kyleledbetter.com>
2016-07-18 16:13:51 -0500

    feature(layouts): responsive nav-list (#21)
    * udpate(nav-list): make list nav responsive
    
    * udpate(layouts-overview): add images
    
    * update(nav-list): min-width for list
    
    * update(layout): keep list visible on desktop
    
    * update(docs): add list toggle
    
    * update(layout-nav-list): better responsive actions
    
    * update(navs): toggle list
    
    * update(manage-list): hide filters sidenav
    
    * update(layouts): remove back-arrow button
    
    - add card footer actions instead
    
    * postinstall webdriver-manager update
    
    * travis failing with webdriver update, separate in 2 steps

--- src/app/components/layouts/card-over/card-over.component.ts
+++ src/app/components/layouts/card-over/card-over.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { MdButton } from '@angular2-material/button';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { MdIcon } from '@angular2-material/icon';
@@ -11,13 +10,14 @@ import { TdHighlightComponent } from '../../../../platform/highlight';
 
 @Component({
   directives: [
-    MdButton,
+    MD_BUTTON_DIRECTIVES,
     MD_CARD_DIRECTIVES,
     MD_LIST_DIRECTIVES,
     MdIcon,
     TD_LAYOUT_DIRECTIVES,
     TdHighlightComponent,
     TD_STEPS_DIRECTIVES,
+    ROUTER_DIRECTIVES,
   ],
   moduleId: module.id,
   selector: 'td-layouts-card-over',
@@ -26,9 +26,4 @@ import { TdHighlightComponent } from '../../../../platform/highlight';
 })
 export class CardOverComponent {
 
-  constructor(private router: Router) {}
-
-  goBack(): void {
-    this.router.navigate(['/layouts']);
-  }
 }
