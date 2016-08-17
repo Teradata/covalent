42612660795b4945e63d0e1ce52aeeec1bfbd9d1
richa <richa.vyas@teradata.com>
2016-08-18 01:07:41 +0530

    multi line chart support; updated docs; modified sample files; colors passed as config

--- src/platform/charts/line-chart/line-chart.component.ts
+++ src/platform/charts/line-chart/line-chart.component.ts
@@ -8,12 +8,16 @@ declare var d3: any;
   styleUrls: ['line-chart.component.css'],
   templateUrl: 'line-chart.component.html',
 })
+
 export class TdLineChartComponent implements AfterViewInit {
 
   private _margin = {top: 50, right: 150, bottom: 50, left: 50};
   private _width: number;
   private _height: number;
   private _padding: number;
+  private _lineColumns: string[] = [];
+  private _lineTitles: string[] = [];
+  private _lineColors: string[] = [];
 
   @ViewChild('linechart') content: ElementRef;
 
@@ -29,20 +33,40 @@ export class TdLineChartComponent implements AfterViewInit {
   @Input('contentType') contentType: string = '';
 
   /**
-   * xName?: string.
+   * bottomAxis?: string.
    */
-  @Input('xName') xName: string = '';
+  @Input('bottomAxis') bottomAxis: string = '';
 
   /**
-   ** xName?: string.
+   * bottomAxisTitle?: string.
    */
-  @Input('yName') yName: string = '';
+  @Input('bottomAxisTitle') bottomAxisTitle: string = '';
 
   /**
-   * title?: string.
+   ** leftAxisTitle?: string.
+   */
+  @Input('leftAxisTitle') leftAxisTitle: string = '';
+
+  /**
+   * chartTitle?: string.
    * Title of the Chart
    */
-  @Input('title') title: string = '';
+  @Input('chartTitle') chartTitle: string = '';
+
+  @Input('lineColumns')
+  set lineColumns(lineColumns: string[]) {
+    this._lineColumns = lineColumns;
+  }
+
+  @Input('lineTitles')
+  set lineTitles(lineTitles: string[]) {
+    this._lineTitles = lineTitles;
+  }
+
+  @Input('lineColors')
+  set lineColors(lineColors: string[]) {
+    this._lineColors = lineColors;
+  }
 
   ngAfterViewInit(): void {
     this.render();
@@ -54,19 +78,14 @@ export class TdLineChartComponent implements AfterViewInit {
     this._height = 500 - this._margin.top - this._margin.bottom;
     this._padding = 100;
 
-    var x = d3.scaleLinear()
-      .range([0, this._width]);
+    var x = d3.scaleLinear().range([0, this._width]);
+    var y = d3.scaleLinear().range([this._height, 0]);
+    var color = d3.scaleOrdinal(this._lineColors);
 
-    var y = d3.scaleLinear()
-      .range([this._height, 0]);
-
-    var line = d3.line()
-      .x((d) => {
-        return x(d[this.xName]);
-      })
-      .y((d) => {
-        return y(d[this.yName]);
-      });
+    var drawLine = d3.line()
+      .curve(d3.curveBasis)
+      .x(function(d) { return x(d.xValue); })
+      .y(function(d) { return y(d.yValue); });
 
     let viewBoxWidth = this._width + this._margin.left + this._margin.right;
     let viewBoxHeight = this._height + this._margin.top + this._margin.bottom;
@@ -89,12 +108,28 @@ export class TdLineChartComponent implements AfterViewInit {
     ParseContent[this.contentType](this.filePath, (error, data) => {
       if (error) throw error;
 
+      var lines = this._lineColumns.map((id) => {
+        return {
+          id: id,
+          values: data.map((d) => {
+            return {xValue: d[this.bottomAxis], yValue: d[id]};
+          })
+        };
+      });
+
       x.domain(d3.extent(data, (d) => {
-        return d[this.xName];
+        return d[this.bottomAxis];
       }));
       y.domain(d3.extent(data, (d) => {
-        return d[this.yName];
+        return d[this._lineColumns[0]];
       }));
+
+      x.domain(d3.extent(data, (d) => { return d[this.bottomAxis]; }));
+
+      y.domain([
+        d3.min(lines, function(c) { return d3.min(c.values, function(d) { return d.yValue; }); }),
+        d3.max(lines, function(c) { return d3.max(c.values, function(d) { return d.yValue; }); })
+      ]);
 
       svg.append("g")
         .attr("class", "axis axis--x")
@@ -103,27 +138,36 @@ export class TdLineChartComponent implements AfterViewInit {
 
       svg.append("g")
         .attr("class", "axis axis--y")
-        .call(d3.axisLeft(y));
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("fill", "#000")
+        .text(this.leftAxisTitle);
 
-      svg.append("path")
-        .datum(data)
+      var line = svg.selectAll(".lineTitle")
+        .data(lines)
+        .enter().append("g")
+        .attr("class", "lineTitle");
+
+      line.append("path")
         .attr("class", "line")
-        .attr("d", line);
+        .attr("d", function(d) { return drawLine(d.values); })
+        .style("stroke", function(d) { return color(d.id); });
 
-      svg.append("text")
-        .attr("text-anchor", "middle")
-        .attr("transform", "translate(" + (0 - this._padding / 2 - this._margin.left / 2) + "," + (this._height / 2) + ")")
-        .text(this.yName);
-
-      svg.append("text")
-        .attr("text-anchor", "middle")
-        .attr("transform", "translate(" + (this._width / 2) + "," + (this._height + (this._padding / 2)) + ")")
-        .text(this.xName);
+      line.append("text")
+        .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+        .attr("transform", function(d) { return "translate(" + x(d.value.xValue) + "," + y(d.value.yValue) + ")"; })
+        .attr("x", 3)
+        .attr("dy", "0.35em")
+        .style("font", "10px sans-serif")
+        .text((d,i) => { return this._lineTitles[i];});
 
       svg.append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "translate(" + (this._width / 2) + "," + (0 - (this._margin.top / 2)) + ")")
-        .text(this.title)
+        .text(this.chartTitle)
         .attr("class", "md-title");
 
     });
