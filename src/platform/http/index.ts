42e9561a757d84abfec5f53a4e2de36123c79f63
Ed Morales <eduardo.morales@Teradata.com>
2016-09-12 10:39:42 -0700

    created http.module.ts for CovalentHttpModule

--- src/platform/http/index.ts
+++ src/platform/http/index.ts
@@ -1,35 +1,3 @@
 export { RESTService, IRestTransform, IRestConfig, IRestQuery, IHttp } from './http-rest.service';
 export { IHttpInterceptor, HttpInterceptorService } from './http-interceptor.service';
-
-import { HttpInterceptorService } from './http-interceptor.service';
-
-import { NgModule, ModuleWithProviders, Type, Injector } from '@angular/core';
-import { HttpModule, Http } from '@angular/http';
-
-@NgModule({
-  imports: [
-    HttpModule,
-  ],
-  providers: [
-    HttpInterceptorService,
-  ],
-})
-export class CovalentHttpModule {
-  static forRoot(requestInterceptors: Type<any>[] = []): ModuleWithProviders {
-    let providers: any[] = [];
-    requestInterceptors.forEach((interceptor: Type<any>) => {
-      providers.push(interceptor);
-    });
-    providers.push({
-      provide: HttpInterceptorService,
-      useFactory: (http: Http, injector: Injector): HttpInterceptorService => {
-        return new HttpInterceptorService(http, injector, requestInterceptors);
-      },
-      deps: [Http, Injector],
-    });
-    return {
-      ngModule: CovalentHttpModule,
-      providers: providers,
-    };
-  }
-}
+export { CovalentHttpModule } from './http.module';
