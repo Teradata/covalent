dc90026d762a94d090ac79bbd545e90142d571c2
richa <richa.vyas@teradata.com>
2016-10-07 01:39:41 +0530

    time-series support added; code cleanup

--- src/platform/charts/chart-line/chart-line.component.ts
+++ src/platform/charts/chart-line/chart-line.component.ts
@@ -14,20 +14,9 @@ export class TdChartLineComponent implements AfterViewInit {
   private _margin: any = {top: 50, right: 150, bottom: 50, left: 50};
   private _width: number;
   private _height: number;
-  private _padding: number;
-  private _columns: string[] = [];
-  private _titles: string[] = [];
-  private _colors: string[] = [];
 
-  private _shadowDepth: any[] = [];
-  private _tickHeightSize: number = 0;
-  private _tickWidthSize: number = 0;
-  private _grid: string = '';
-  private _colorPalette: string[] = [];
   private _parentObj: any;
   private _chartTitle: string;
-  private _shadowColor: string;
-  private _fillOpacity: number = 0;
   private _leftAxisTitle: string;
 
   @ViewChild('linechart') content: ElementRef;
@@ -37,76 +26,41 @@ export class TdChartLineComponent implements AfterViewInit {
   @Input() transition: boolean = true;
   @Input() transitionDuration: number;
   @Input() transitionDelay: number;
-  @Input('columns')
-  set columns(columns: string[]) {
-    this._columns = columns;
-  }
-
-  @Input('titles')
-  set titles(titles: string[]) {
-    this._titles = titles;
-  }
-
-  @Input('colors')
-  set colors(colors: string[]) {
-    this._colors = colors;
-  }
+  @Input() timeSeries: boolean = false;
+  @Input() columns: string[];
+  @Input() titles: string[];
+  @Input() colors: string[];
 
   constructor(@Inject(forwardRef(() => TdChartsComponent)) private _parent: TdChartsComponent) {
     this._parentObj = _parent;
   }
 
   ngAfterViewInit(): void {
-    this.render();
+    this._chartTitle = this._parentObj.chartTitle;
+    this._leftAxisTitle = this._parentObj.leftAxisTitle;
+    this.drawLineChart();
   }
 
-  render(): void {
+  drawLineChart(): void {
     this._margin.top = 50;
     this._width = 960 - this._margin.left - this._margin.right;
     this._height = 500 - this._margin.top - this._margin.bottom;
-    this._padding = 100;
 
     let t: any = d3.transition()
             .duration(this.transitionDuration)
             .delay(this.transitionDelay)
             .ease(d3.easeLinear);
 
-    if (this._parentObj.chartTitle) {
-      this._chartTitle = this._parentObj.chartTitle;
-    }
+    let parseTime = d3.timeParse('%d-%b-%y');
+    let stringCol = function(record: any, bottomAxis: string): string { return record[bottomAxis]; }
+    let dateCol = function(record: any, bottomAxis: string): Date { return parseTime(record[bottomAxis]);}
 
-    if (this._parentObj.colorPalette) {
-      this._colorPalette = this._parentObj.colorPalette;
-    }
+    let row = this.timeSeries ? dateCol : stringCol;
+    let xScale = this.timeSeries ? d3.scaleTime : d3.scaleLinear;
 
-    if (this._parentObj.ticks === true) {
-      this._tickHeightSize = -this._height;
-      this._tickWidthSize = -this._width;
-    }
-
-    if (this._parentObj.grid === true) {
-      this._grid = 'grid';
-    }
-
-    if (this._parentObj.shadowDepth) {
-      this._shadowDepth =  this._parentObj.shadowDepth;
-    }
-
-    if (this._parentObj.shadowColor) {
-      this._shadowColor = this._parentObj.shadowColor;
-    }
-
-    if (this._parentObj.fillOpacity) {
-      this._fillOpacity = this._parentObj.fillOpacity;
-    }
-
-    if (this._parentObj.leftAxisTitle) {
-      this._leftAxisTitle = this._parentObj.leftAxisTitle;
-    }
-
-    let x: any = d3.scaleLinear().range([0, this._width]);
+    let x: any = xScale().range([0, this._width]);
     let y: any = d3.scaleLinear().range([this._height, 0]);
-    let color: any = d3.scaleOrdinal(this._colors);
+    let color: any = d3.scaleOrdinal(this.colors);
 
     let drawLine: any = d3.line()
       .curve(d3.curveBasis)
@@ -126,20 +80,18 @@ export class TdChartLineComponent implements AfterViewInit {
     }
 
     ParseContent[this.contentType](this.dataSrc, (error: string, data: any) => {
-      if (error) {
-        throw error;
-      }
+      if (error) {throw error;}
 
-      let lines: Object = this._columns.map((id: any) => {
+      let lines: Object = this.columns.map((id: any) => {
         return {
           id: id,
           values: data.map((d: any) => {
-            return {xValue: d[this.bottomAxis], yValue: d[id]};
+            return {xValue: row(d, this.bottomAxis), yValue: +d[id]};
           }),
         };
       });
 
-      x.domain(d3.extent(data, (d: any) => { return d[this.bottomAxis]; }));
+      x.domain(d3.extent(data, (d: any) => { return row(d, this.bottomAxis); }));
 
       y.domain([
         d3.min(lines, (c: any) => { return d3.min(c.values, (d: any) => { return d.yValue; }); }),
@@ -179,14 +131,13 @@ export class TdChartLineComponent implements AfterViewInit {
         .attr('x', 3)
         .attr('dy', '0.35em')
         .style('font', '10px sans-serif')
-        .text((d: any, i: number) => { return this._titles[i]; });
+        .text((d: any, i: number) => { return this.titles[i]; });
 
       svg.append('text')
         .attr('text-anchor', 'middle')
         .attr('transform', 'translate(' + (this._width / 2) + ',' + (0 - (this._margin.top / 2)) + ')')
         .text(this._chartTitle)
         .attr('class', 'md-title');
-
     });
   }
 }
