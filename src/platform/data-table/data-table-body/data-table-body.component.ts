6ed0e3e1b1a4c5b696c941ab811495636671fb4e
Ed Morales <eduardo.morales@Teradata.com>
2016-10-29 15:20:35 -0700

    removed td-body and added td-data-table-body component

---
+++ src/platform/data-table/data-table-body/data-table-body.component.ts
@@ -0,0 +1,14 @@
+import { Component, Renderer, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'tbody[td-data-table-body]',
+  styleUrls: [ 'data-table-body.component.scss' ],
+  templateUrl: 'data-table-body.component.html',
+})
+export class TdDataTableBodyComponent {
+
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer) {
+    this._renderer.setElementClass(this._elementRef.nativeElement, 'md-body', true);
+  }
+
+}
