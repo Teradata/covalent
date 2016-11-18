4ee33c74dcff764125a9bba2ad84e37d0aaf0721
Ed Morales <emoralesb05@gmail.com>
2016-11-18 13:21:20 -0800

    feature(dialog): close blocking & enhancements (#144)
    * upgrade angular to 2.1.2
    
    * fixed empty input since angular complains now https://github.com/angular/angular/issues/12768
    
    * update module dependencies
    
    * update to latest angular-cli
    
    * upgrade to alpha.10 and breaking changes
    
    * added disableClose property for dialogs
    
    * removes validation for viewContainerRef since dialogs no longer require it
    
    * no more need for setDefaultViewContainerRef since viewContainerRef is not required for dialogs
    
    * updated docs with disableClose and remove setDefault example
    
    * setDefaultViewContainerRef deprecated
    
    * removed not required code for dialogs and snackbars
    
    * fixed validation messages since md-hint overrides display:none from [hidden]
    
    * update example in form validations
    
    * update(inline-errors): better error message
    
    * fixed spelling for address variables

--- src/platform/core/dialogs/services/dialog.service.ts
+++ src/platform/core/dialogs/services/dialog.service.ts
@@ -9,6 +9,7 @@ export interface IDialogConfig {
   title?: string;
   message: string;
   viewContainerRef?: ViewContainerRef;
+  disableClose?: boolean;
 }
 
 export interface IAlertConfig extends IDialogConfig {
@@ -27,18 +28,17 @@ export interface IPromptConfig extends IConfirmConfig {
 @Injectable()
 export class TdDialogService {
 
-  private _viewContainerRef: ViewContainerRef;
-
   constructor(private _dialogService: MdDialog) {}
 
   /**
    * params:
    * - viewContainerRef: ViewContainerRef
-   * 
    * Sets a detaulf ViewContainerRef object to which all dialogs will belong to.
+   * @deprecated since 0.9
    */
   public setDefaultViewContainerRef(viewContainerRef: ViewContainerRef): void {
-    this._viewContainerRef = viewContainerRef;
+    /* tslint:disable-next-line */ 
+    console.warn('setDefaultViewContainerRef is deprecated. ViewContainerRef is no longer required.');
   }
 
   /**
@@ -54,7 +54,7 @@ export class TdDialogService {
    * Returns an MdDialogRef<TdAlertDialogComponent> object.
    */
   public openAlert(config: IAlertConfig): MdDialogRef<TdAlertDialogComponent> {
-    let dialogConfig: MdDialogConfig = this._createConfig(config.viewContainerRef);
+    let dialogConfig: MdDialogConfig = this._createConfig(config);
     let dialogRef: MdDialogRef<TdAlertDialogComponent> =
       this._dialogService.open(TdAlertDialogComponent, dialogConfig);
     let alertDialogComponent: TdAlertDialogComponent = dialogRef.componentInstance;
@@ -80,7 +80,7 @@ export class TdDialogService {
    * Returns an MdDialogRef<TdConfirmDialogComponent> object.
    */
   public openConfirm(config: IConfirmConfig): MdDialogRef<TdConfirmDialogComponent> {
-    let dialogConfig: MdDialogConfig = this._createConfig(config.viewContainerRef);
+    let dialogConfig: MdDialogConfig = this._createConfig(config);
     let dialogRef: MdDialogRef<TdConfirmDialogComponent> =
       this._dialogService.open(TdConfirmDialogComponent, dialogConfig);
     let confirmDialogComponent: TdConfirmDialogComponent = dialogRef.componentInstance;
@@ -110,7 +110,7 @@ export class TdDialogService {
    * Returns an MdDialogRef<TdPromptDialogComponent> object.
    */
   public openPrompt(config: IPromptConfig): MdDialogRef<TdPromptDialogComponent> {
-    let dialogConfig: MdDialogConfig = this._createConfig(config.viewContainerRef);
+    let dialogConfig: MdDialogConfig = this._createConfig(config);
     let dialogRef: MdDialogRef<TdPromptDialogComponent> =
       this._dialogService.open(TdPromptDialogComponent, dialogConfig);
     let promptDialogComponent: TdPromptDialogComponent = dialogRef.componentInstance;
@@ -126,12 +126,10 @@ export class TdDialogService {
     return dialogRef;
   }
 
-  private _createConfig(viewContainerRef: ViewContainerRef): MdDialogConfig {
+  private _createConfig(config: MdDialogConfig): MdDialogConfig {
     let dialogConfig: MdDialogConfig = new MdDialogConfig();
-    dialogConfig.viewContainerRef = viewContainerRef ? viewContainerRef : this._viewContainerRef;
-    if (!dialogConfig.viewContainerRef) {
-      throw 'ViewContainerRef was not provided for dialog.';
-    }
+    dialogConfig.viewContainerRef = config.viewContainerRef;
+    dialogConfig.disableClose = config.disableClose;
     return dialogConfig;
   }
 
