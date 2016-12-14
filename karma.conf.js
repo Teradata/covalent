8798b55c3139cbc130e64c5096d89fe37154b093
Ed Morales <eduardo.morales@Teradata.com>
2016-12-14 12:51:29 -0800

    adding dist: trustry to see if it works in travis

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function (config) {
     singleRun: false
   };
   if (process.env.TRAVIS) {
-    configuration.browsers = ['Firefox'];//['Chrome_travis_ci'];
+    configuration.browsers = ['Chrome_travis_ci'];
   }
   config.set(configuration);
 };
