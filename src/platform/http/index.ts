7a627c9d2aece7239cabeef44e6920fb2b245419
Ed Morales <eduardo.morales@Teradata.com>
2016-12-14 22:42:16 -0800

    fixed index.ts to export correct interfaces

--- src/platform/http/index.ts
+++ src/platform/http/index.ts
@@ -1,3 +1,4 @@
 export { RESTService, IRestTransform, IRestConfig, IRestQuery, IHttp } from './http-rest.service';
-export { IHttpInterceptor, HttpInterceptorService } from './http-interceptor.service';
+export { HttpInterceptorService, IHttpInterceptorConfig } from './interceptors/http-interceptor.service';
+export { IHttpInterceptor } from './interceptors/http-interceptor.interface';
 export { CovalentHttpModule } from './http.module';
