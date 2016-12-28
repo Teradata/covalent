25a6e814f48d2f3f33277b5e12a9575786236347
Kyle Ledbetter <kyle.ledbetter@teradata.com>
2016-12-28 16:39:28 -0600

    update(footer): disable lint for multiple selectors

--- src/platform/core/layout/layout-footer/layout-footer.component.ts
+++ src/platform/core/layout/layout-footer/layout-footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  /* tslint:disable-next-line */
   selector: 'td-layout-footer,td-layout-footer-inner',
   styleUrls: ['./layout-footer.component.scss' ],
   templateUrl: './layout-footer.component.html',
