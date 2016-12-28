3205695f093195f79c97f4179fdaa8602fe47c3e
richa <richa.vyas@teradata.com>
2016-12-28 13:07:48 -0800

    removed extra space from selector name

--- src/platform/core/layout/layout-footer/layout-footer.component.ts
+++ src/platform/core/layout/layout-footer/layout-footer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'td-layout-footer, td-layout-footer-inner',
+  selector: 'td-layout-footer,td-layout-footer-inner',
   styleUrls: ['./layout-footer.component.scss' ],
   templateUrl: './layout-footer.component.html',
 })
