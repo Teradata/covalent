15bff8fc82e93c917f0f36445297ad0a4bfa6e93
emoralesb05 <emoralesb05@gmail.com>
2017-01-16 10:33:14 -0800

    add display animation change on fadeInOut animation

--- src/platform/core/common/animations/fade/fadeInOut.animation.ts
+++ src/platform/core/common/animations/fade/fadeInOut.animation.ts
@@ -14,9 +14,11 @@ export function TdFadeInOutAnimation(duration: number = 150): AnimationEntryMeta
   return trigger('tdFadeInOut', [
     state('false', style({
       opacity: '0',
+      display: 'none',
     })),
     state('true',  style({
       opacity: '*',
+      display: '*',
     })),
     transition('0 => 1', animate(duration + 'ms ease-in')),
     transition('1 => 0', animate(duration + 'ms ease-out')),
