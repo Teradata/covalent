988940253db3df545f1e19ee119cc2c4dee19605
Ed Morales <emoralesb05@gmail.com>
2017-01-21 10:55:28 -0800

    feature(markdown): enhancements and code-health. (#276)
    * feature(markup): support indentation spaces in html code.
    
    * feature(markdown): add <pre><code> by default
    
    * remove note since its not needed anymore and update docs
    
    * prepare README.md to be included in docs
    
    * added sublist examples
    
    * added [content] input to `td-markdown` to load content dynamically
    
    * refactor code a bit and sanatize `html` markdown by using [innerHTML]
    
    * replaced docs with markdown/README.md to unify documentation
    
    * use container, renderer and domSanitizer to prevent XSS issues, reduce markup and limiting acces to native DOM
    
    * update README.md with NOTE about the sanitazion warning
    
    * clear innerHTML with renderer too
    
    * code-health(markup): initial unit tests for markdown module
    
    * rename unit test
    
    * polish for loop to make it more simple
    
    * fix(markdown): hr now showing properly
    
    * update(docs): component subheaders in left nav
    
    * update(docs): add demo/code tabs to markdown

--- src/platform/markdown/markdown.component.ts
+++ src/platform/markdown/markdown.component.ts
@@ -1,4 +1,5 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Input, Renderer, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 declare var showdown: any;
 
@@ -9,18 +10,82 @@ declare var showdown: any;
 })
 export class TdMarkdownComponent implements AfterViewInit {
 
-  @ViewChild('markdown') content: ElementRef;
+  private _content: string;
 
-  ngAfterViewInit(): void {
-    let code: string = this.content.nativeElement.innerText;
-    this.content.nativeElement.innerHTML = this.render(code);
+  /**
+   * content?: string
+   * 
+   * Markdown format content to be parsed as html markup.
+   * Used to load data dynamically.
+   * 
+   * e.g. Resource file.
+   */
+  @Input('content')
+  set content(content: string) {
+    this._content = content;
+    this._loadContent(this._content);
   }
 
-  render(contents: string): string {
+  constructor(private _renderer: Renderer,
+              private _elementRef: ElementRef,
+              private _domSanitizer: DomSanitizer) {}
+
+  ngAfterViewInit(): void {
+    if (!this._content) {
+      this._loadContent((<HTMLElement>this._elementRef.nativeElement).textContent);
+    }
+  }
+
+  /**
+   * General method to parse a string markdown into HTML Elements and load them into the container
+   */
+  private _loadContent(markdown: string): void {
+    if (markdown && markdown.trim().length > 0) {
+      // Parse html string into actual HTML elements.
+      let divElement: HTMLDivElement = this._elementFromString(this._render(markdown));
+      // Clean container
+      this._renderer.setElementProperty(this._elementRef.nativeElement, 'innerHTML', '');
+      // Project DIV element into container
+      this._renderer.projectNodes(this._elementRef.nativeElement, [divElement]);
+    }
+  }
+
+  private _elementFromString(markupStr: string): HTMLDivElement {
+    // Renderer doesnt have a parsing method, so we have to sanitize and use [innerHTML]
+    // to parse the string into DOM element for now.
+    const div: HTMLDivElement = this._renderer.createElement(this._elementRef.nativeElement, 'div');
+    div.innerHTML = this._domSanitizer.sanitize(SecurityContext.HTML, markupStr);
+    return div;
+  }
+
+  private _render(markup: string): string {
+    // Split markup by line characters
+    let lines: string[] = markup.split('\n');
+
+    // check how much indentation is used by the first actual markup line
+    let firstLineWhitespace: string = '';
+    for (let line of lines) {
+      if (line && line.trim().length > 0) {
+        firstLineWhitespace = line.match(/^\s*/)[0];
+        break;
+      }
+    }
+
+    // Remove all indentation spaces so markup can be parsed correctly
+    let startingWhitespaceRegex: RegExp = new RegExp('^' + firstLineWhitespace);
+    lines = lines.map(function(line: string): string {
+      return line.replace(startingWhitespaceRegex, '');
+    });
+
+    // Join lines again with line characters
+    let codeToParse: string =  lines.join('\n');
+
+    // Convert markup into html
     let converter: any = new showdown.Converter();
     converter.setOption('ghCodeBlocks', true);
     converter.setOption('tasklists', true);
-    let html: string = converter.makeHtml(contents);
+    converter.setOption('tables', true);
+    let html: string = converter.makeHtml(codeToParse);
     return html;
   }
 
