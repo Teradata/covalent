110eba663f4cd582f0c99a3fb038d7cb342cc4df
emoralesb05 <emoralesb05@gmail.com>
2017-01-20 21:17:02 -0800

    feature(markup): support indentation spaces in html code.

--- src/platform/markdown/markdown.component.ts
+++ src/platform/markdown/markdown.component.ts
@@ -12,15 +12,38 @@ export class TdMarkdownComponent implements AfterViewInit {
   @ViewChild('markdown') content: ElementRef;
 
   ngAfterViewInit(): void {
-    let code: string = this.content.nativeElement.innerText;
-    this.content.nativeElement.innerHTML = this.render(code);
+    let markup: string = this.content.nativeElement.innerText;
+    this.content.nativeElement.innerHTML = this.render(markup);
   }
 
-  render(contents: string): string {
+  render(markup: string): string {
+    // Split markup by line characters
+    let lines: string[] = markup.split('\n');
+
+    // check how much indentation is used by the first actual markup line
+    let whiteSpacesInfront: number = 0;
+    for (let i: number = 0; i < lines.length; i++) {
+      let line: string = lines[i];
+      if (line.trim().length > 0) {
+        whiteSpacesInfront = line.match(/^\s*/)[0].length;
+        break;
+      }
+    }
+
+    // Remove all indentation spaces so markup can be parsed correctly
+    let startingWhitespaceRegex: RegExp = new RegExp('^\\s{' + whiteSpacesInfront + '}');
+    lines = lines.map(function(line: string): string {
+      return line.replace(startingWhitespaceRegex, '');
+    });
+
+    // Join lines again with line characters
+    let codeToParse: string =  lines.join('\n');
+
+    // Convert markup into html
     let converter: any = new showdown.Converter();
     converter.setOption('ghCodeBlocks', true);
     converter.setOption('tasklists', true);
-    let html: string = converter.makeHtml(contents);
+    let html: string = converter.makeHtml(codeToParse);
     return html;
   }
 
