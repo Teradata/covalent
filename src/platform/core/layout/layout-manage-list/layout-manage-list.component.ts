e655ccf4b7894da8e4efd9b285452ee56c0e3622
emoralesb05 <emoralesb05@gmail.com>
2017-01-25 21:40:35 -0800

    feature(layout-manage-list): Added inputs [opened], [mode], [locked], and [sidenavWidth] to allow more flexibility (with media service) + removed fixed breakpoint for mobile + polished code

--- src/platform/core/layout/layout-manage-list/layout-manage-list.component.ts
+++ src/platform/core/layout/layout-manage-list/layout-manage-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 
-import { MdSidenav } from '@angular/material';
+import { MdSidenav, MdSidenavToggleResult } from '@angular/material';
 
 @Component({
   selector: 'td-layout-manage-list',
@@ -12,23 +12,70 @@ export class TdLayoutManageListComponent {
   @ViewChild(MdSidenav) _sideNav: MdSidenav;
 
   /**
+   * mode?: 'side', 'push' or 'over'
+   * The mode or styling of the sidenav.
+   * Defaults to "side".
+   * See "MdSidenav" documentation for more info.
+   * 
+   * https://github.com/angular/material2/tree/master/src/lib/sidenav
+   */
+  @Input('mode') mode: 'side' | 'push' | 'over' = 'side';
+
+  /**
+   * locked?: boolean
+   * Whether or not the sidenav is closed when clicking on its [td-sidenav-content] elements.
+   * If "true", it will remain open. 
+   * 
+   * Defaults to "true".
+   */
+  @Input('locked') locked: boolean = true;
+
+  /**
+   * opened?: boolean
+   * Whether or not the sidenav is opened. Use this binding to open/close the sidenav.
+   * Defaults to "true".
+   * 
+   * See "MdSidenav" documentation for more info.
+   * 
+   * https://github.com/angular/material2/tree/master/src/lib/sidenav
+   */
+  @Input('opened') opened: boolean = true;
+
+  // Cant support % yet as stated in the layout docs
+  /**
+   * sidenavWidth?: string
+   * Sets the "width" of the sidenav in either "px" or "%" ("%" is not well supported yet as stated in the layout docs)
+   * Defaults to "257px".
+   * 
+   * https://github.com/angular/material2/tree/master/src/lib/sidenav
+   */
+  @Input('sidenavWidth') sidenavWidth: string = '257px';
+
+  /**
    * Proxy toggle method to access sidenav from outside (from td-layout template).
    */
-  public toggle(): void {
-    this._sideNav.toggle();
+  public toggle(): Promise<MdSidenavToggleResult> {
+    return this._sideNav.toggle();
   }
 
   /**
    * Proxy open method to access sidenav from outside (from td-layout template).
    */
-  public open(): void {
-    this._sideNav.open();
+  public open(): Promise<MdSidenavToggleResult> {
+    return this._sideNav.open();
   }
 
   /**
    * Proxy close method to access sidenav from outside (from td-layout template).
    */
-  public close(): void {
-    this._sideNav.close();
+  public close(): Promise<MdSidenavToggleResult> {
+    return this._sideNav.close();
+  }
+
+  /**
+   * Returns the instance of the underlying [MdSidenav].
+   */
+  public sidenav(): MdSidenav {
+    return this._sideNav;
   }
 }
