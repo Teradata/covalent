548619860f2a20dc51502ac39e91becd5c195586
emoralesb05 <emoralesb05@gmail.com>
2017-01-25 20:53:11 -0800

    polished layout-nav component

--- src/platform/core/layout/layout-nav/layout-nav.component.ts
+++ src/platform/core/layout/layout-nav/layout-nav.component.ts
@@ -10,26 +10,36 @@ import { TdLayoutComponent } from '../layout.component';
 export class TdLayoutNavComponent {
 
   /**
-   * title in toolbar
+   * title?: string 
+   * Title set in toolbar.
    */
   @Input('toolbarTitle') toolbarTitle: string;
 
   /**
-   * icon for toolbar
+   * icon?: string
+   * icon name to be displayed before the title
    */
   @Input('icon') icon: string;
 
   /**
-   * logo file for toolbar
+   * logo?: string
+   * logo icon name to be displayed before the title.
+   * If [icon] is set, then this will not be shown.
    */
   @Input('logo') logo: string;
 
+  /**
+   * Checks if there is a [TdLayoutComponent] as parent.
+   */
   get isMainSidenavAvailable(): boolean {
     return !!this._layout;
   }
 
   constructor(@Optional() @Inject(forwardRef(() => TdLayoutComponent)) private _layout: TdLayoutComponent) {}
 
+  /**
+   * If main sidenav is available, it will open the sidenav of the parent [TdLayoutComponent].
+   */
   openMainSidenav(): void {
     if (this.isMainSidenavAvailable) {
       this._layout.open();
