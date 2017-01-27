f6394731a4593beb7daf0176fc322126a1af53f6
emoralesb05 <eduardo.morales@teradata.com>
2017-01-27 01:08:32 -0800

    Added service to td-layout so it could be used also

--- src/platform/core/layout/layout.component.ts
+++ src/platform/core/layout/layout.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { MdSidenav, MdSidenavToggleResult } from '@angular/material';
+
+import { TdLayoutService } from './services/layout.service';
 
 @Component({
   selector: 'td-layout',
   styleUrls: ['./layout.component.scss' ],
   templateUrl: './layout.component.html',
 })
-export class TdLayoutComponent {
+export class TdLayoutComponent implements OnInit, OnDestroy {
 
+  private _registerSubscription: Subscription;
   @ViewChild(MdSidenav) _sideNav: MdSidenav;
 
   /**
@@ -29,6 +33,21 @@ export class TdLayoutComponent {
    * If [icon] is set, then this will not be shown.
    */
   @Input('logo') logo: string;
+
+  constructor(private _layoutService: TdLayoutService) {}
+
+  ngOnInit(): void {
+    this._registerSubscription = this._layoutService.registerSidenav().subscribe(() => {
+      this.open();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this._registerSubscription) {
+      this._registerSubscription.unsubscribe();
+      this._registerSubscription = undefined;
+    }
+  }
 
   /**
    * Proxy toggle method to access sidenav from outside (from td-layout template).
