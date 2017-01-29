2b605831c501b743189dc7d24a91c3dc5445b8b7
Ed Morales <emoralesb05@gmail.com>
2017-01-29 14:47:51 -0800

    internal(build): improvements and bugfixes around our `build` and `publishing` process. (#296)
    * bugfix(build): sourcemap was being generated with an absolute route because of the `mapRoot` property
    
    * internal(build): leverage `skipTemplateCodegen` from the `angularCompilerOptions` + do `aot` after the resources are inlined
    
    * internal(build): leverage the `ngc` to generate our `ts` complation from the `tsconfig-aot.json` + inline `ts` files before compilation starts
    
    * better messages for build process
    
    * return to only inline resources in js after `aot` build but replace `scss` with `css` in `ts` files before `aot`
    
    * return to `"` wrap just in case

--- scripts/inline-resource-files.js
+++ scripts/inline-resource-files.js
@@ -4,6 +4,6 @@ var gulp = require('gulp-help')(require('gulp'));
 var inlineResources = require('./inline-resources.js');
 var config = require('../build.conf');
 
-gulp.task('inline-resource-files', 'Inline resouces files into JS files', function() {
+gulp.task('inline-resource-files', 'Inline resouces files into the files that will be published', function() {
   inlineResources([config.paths.deployed]);
 });
