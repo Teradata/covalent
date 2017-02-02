2092ba2d5ab89727ab01ba67441b9e21984be09b
emoralesb05 <eduardo.morales@teradata.com>
2017-02-01 21:35:05 -0800

    forgot to update the `karma.conf.js` to point to `@angular/cli`

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,13 @@
 module.exports = function (config) {
   var configuration = {
     basePath: '.',
-    frameworks: ['jasmine', 'angular-cli'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-remap-istanbul'),
-      require('angular-cli/plugins/karma')
+      require('@angular/cli/plugins/karma')
     ],
     customLaunchers: {
       // chrome setup for travis CI using chromium
@@ -23,7 +23,7 @@ module.exports = function (config) {
       { pattern: './src/test.ts', watched: false }
     ],
     preprocessors: {
-      './src/test.ts': ['angular-cli']
+      './src/test.ts': ['@angular/cli']
     },
     remapIstanbulReporter: {
       reports: {
