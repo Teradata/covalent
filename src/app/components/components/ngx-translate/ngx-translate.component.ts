6d001944ff4ccc08e26b989c8098b5f4bd30831c
emoralesb05 <eduardo.morales@teradata.com>
2017-02-06 21:18:21 -0800

    added first demo

--- src/app/components/components/ngx-translate/ngx-translate.component.ts
+++ src/app/components/components/ngx-translate/ngx-translate.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { TranslateService } from 'ng2-translate';
+
+import { TRANSLATE_STORAGE_KEY } from '../../../utilities/translate';
+
 @Component({
   selector: 'ngx-translate-demo',
   styleUrls: ['./ngx-translate.component.scss'],
   templateUrl: './ngx-translate.component.html',
 })
 export class NgxTranslateDemoComponent {
+
+  selectedLanguage: string;
+  valueCountForPlural: number = 0;
+  numberToFormat: number = 1000100.20;
+  dateToFormat: Date = new Date();
+
+  constructor(private translate: TranslateService) {
+    this.selectedLanguage = translate.currentLang;
+  }
+
+  languageChange(value: string): void {
+    sessionStorage.setItem(TRANSLATE_STORAGE_KEY, value);
+    location.reload();
+  }
 
 }
--- src/app/utilities/translate.ts
+++ src/app/utilities/translate.ts
@@ -2,11 +2,13 @@ import { Http } from '@angular/http';
 
 import { TranslateService, TranslateStaticLoader, TranslateLoader } from 'ng2-translate';
 
+export const TRANSLATE_STORAGE_KEY: string = 'ngx-translate-lang';
+
 /**
  * Utility method to get selected language from sessionStorage or browser
  */
 export function getSelectedLanguage(translateService: TranslateService): string {
-  let storedLanguage: string = sessionStorage.getItem('ngx-translate-lang');
+  let storedLanguage: string = sessionStorage.getItem(TRANSLATE_STORAGE_KEY);
   // Check if the lenguage has been stored else if the language wasnt stored, then use browser default if supported
   if (storedLanguage && translateService.getLangs().indexOf(storedLanguage) > -1) {
     return storedLanguage;
