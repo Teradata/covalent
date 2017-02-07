45b08adc705187d636bb647ee01f909ae48a9c27
emoralesb05 <eduardo.morales@teradata.com>
2017-02-06 20:21:47 -0800

    Added utility methods for `ngx-translate`

---
+++ src/app/utilities/translate.ts
@@ -0,0 +1,25 @@
+import { Http } from '@angular/http';
+
+import { TranslateService, TranslateStaticLoader, TranslateLoader } from 'ng2-translate';
+
+/**
+ * Utility method to get selected language from sessionStorage or browser
+ */
+export function getSelectedLanguage(translateService: TranslateService): string {
+  let storedLanguage: string = sessionStorage.getItem('ngx-translate-lang');
+  // Check if the lenguage has been stored else if the language wasnt stored, then use browser default if supported
+  if (storedLanguage && translateService.getLangs().indexOf(storedLanguage) > -1) {
+    return storedLanguage;
+  } else if (translateService.getLangs().indexOf(translateService.getBrowserLang()) > -1) {
+    return translateService.getBrowserLang();
+  }
+  // If everything fails, then use default lang
+  return translateService.getDefaultLang();
+}
+
+/**
+ * Crate custom TranslateLoader since we have a diff dir structure for our json files
+ */
+export function createTranslateLoader(http: Http): TranslateLoader {
+  return new TranslateStaticLoader(http, 'app/assets/i18n', '.json');
+}
