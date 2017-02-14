e42b5626dad0f7787f25d8dc1f9ea14e395b82fd
emoralesb05 <eduardo.morales@teradata.com>
2017-02-14 15:21:22 -0800

    docs(file-input): add demo and docs for file input

--- src/app/components/components/file-input/file-input.component.ts
+++ src/app/components/components/file-input/file-input.component.ts
@@ -14,25 +14,7 @@ export class FileInputDemoComponent {
   @HostBinding('@routeAnimation') routeAnimation: boolean = true;
   @HostBinding('class.td-route-animation') classAnimation: boolean = true;
 
-  fileSelectMsg: string = 'No file selected yet.';
-  fileSelectMultipleMsg: string = 'No file(s) selected yet.';
   disabled: boolean = false;
-
-  selectEvent(file: File): void {
-    this.fileSelectMsg = file.name;
-  };
-
-  selectMultipleEvent(files: FileList | File): void {
-    if (files instanceof FileList) {
-      let names: string[] = [];
-      for (let i: number = 0; i < files.length; i++) {
-        names.push(files[i].name);
-      }
-      this.fileSelectMultipleMsg = names.join(',');
-    } else {
-      this.fileSelectMultipleMsg = files.name;
-    }
-  }
 
   toggleDisabled(): void {
     this.disabled = !this.disabled;
