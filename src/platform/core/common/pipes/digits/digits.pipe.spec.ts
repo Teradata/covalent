0f0e5b0f74d14b3bdc426a3bf5dd2831c63d307a
emoralesb05 <eduardo.morales@teradata.com>
2017-02-24 17:34:34 -0800

    feat(digits): i10n support for TdDigitsPipe
    leverage LOCALE_ID and DecimalPipe to support l10n within TdDigitsPipe and update unit tests

--- src/platform/core/common/pipes/digits/digits.pipe.spec.ts
+++ src/platform/core/common/pipes/digits/digits.pipe.spec.ts
@@ -2,8 +2,10 @@ import { TdDigitsPipe } from './digits.pipe';
 
 describe('TdDigitsPipe', () => {
   let pipe: TdDigitsPipe;
+  let l10nPipe: TdDigitsPipe;
   beforeEach(() => {
     pipe = new TdDigitsPipe();
+    l10nPipe = new TdDigitsPipe('es');
   });
 
   it('should return with an empty or invalid input', () => {
@@ -13,9 +15,10 @@ describe('TdDigitsPipe', () => {
   });
 
   it('should return formatted digits', () => {
+    /* transformations in 'en'*/
     expect(pipe.transform('34', undefined)).toEqual('34');
-    expect(pipe.transform(0.45, undefined)).toEqual(0.45);
-    expect(pipe.transform(0.724, undefined)).toEqual(0.724);
+    expect(pipe.transform(0.45, 1)).toEqual('0.5');
+    expect(pipe.transform(0.724, 2)).toEqual('0.72');
     expect(pipe.transform(535, undefined)).toEqual('535');
     expect(pipe.transform(138540, undefined)).toEqual('138.5 K');
     expect(pipe.transform(138540, 2)).toEqual('138.54 K');
@@ -26,5 +29,20 @@ describe('TdDigitsPipe', () => {
     expect(pipe.transform(3.81861e+10, undefined)).toEqual('38.2 B');
     expect(pipe.transform(1.890381861e+14, undefined)).toEqual('189 T');
     expect(pipe.transform(5.35765e+16, undefined)).toEqual('53.6 Q');
+
+    /* transformations in 'es'*/
+    expect(l10nPipe.transform('34', undefined)).toEqual('34');
+    expect(l10nPipe.transform(0.45, 1)).toEqual('0,5');
+    expect(l10nPipe.transform(0.724, 2)).toEqual('0,72');
+    expect(l10nPipe.transform(535, undefined)).toEqual('535');
+    expect(l10nPipe.transform(138540, undefined)).toEqual('138,5 K');
+    expect(l10nPipe.transform(138540, 2)).toEqual('138,54 K');
+    expect(l10nPipe.transform(1571800, undefined)).toEqual('1,6 M');
+    expect(l10nPipe.transform(1571800, 3)).toEqual('1,572 M');
+    expect(l10nPipe.transform(10000000, undefined)).toEqual('10 M');
+    expect(l10nPipe.transform(10200000, undefined)).toEqual('10,2 M');
+    expect(l10nPipe.transform(3.81861e+10, undefined)).toEqual('38,2 B');
+    expect(l10nPipe.transform(1.890381861e+14, undefined)).toEqual('189 T');
+    expect(l10nPipe.transform(5.35765e+16, undefined)).toEqual('53,6 Q');
   });
 });
