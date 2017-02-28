6a4c11afad83a41271c698f76bdc621d75f93072
emoralesb05 <eduardo.morales@teradata.com>
2017-02-27 21:51:22 -0800

    chore(): deprecate forRoot in dialog module

--- src/platform/core/dialogs/dialogs.module.ts
+++ src/platform/core/dialogs/dialogs.module.ts
@@ -46,15 +46,25 @@ export { TdDialogService, TdDialogComponent, TdDialogTitleDirective,
   exports: [
     TD_DIALOGS,
   ],
+  providers: [
+    TdDialogService,
+  ],
   entryComponents: [
     TD_DIALOGS_ENTRY_COMPONENTS,
   ],
 })
 export class CovalentDialogsModule {
+  /**
+   * @deprecated in 1.0.0-beta.3
+   *
+   * Please use without calling forRoot()
+   */
   static forRoot(): ModuleWithProviders {
+    /* tslint:disable-next-line */
+    console.warn('forRoot() has been deprecated in CovalentDialogsModule');
     return {
       ngModule: CovalentDialogsModule,
-      providers: [ TdDialogService ],
+      providers: [ ],
     };
   }
 }
