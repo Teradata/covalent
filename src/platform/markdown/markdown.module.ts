95113c0bc230c057e039cbddca69f17e86c8731e
emoralesb05 <eduardo.morales@teradata.com>
2017-02-27 21:44:16 -0800

    chore(): deprecate forRoot() in markdown module

--- src/platform/markdown/markdown.module.ts
+++ src/platform/markdown/markdown.module.ts
@@ -15,7 +15,14 @@ import { TdMarkdownComponent } from './markdown.component';
   ],
 })
 export class CovalentMarkdownModule {
+  /**
+   * @deprecated in 1.0.0-beta.3
+   *
+   * Please use without calling forRoot()
+   */
   static forRoot(): ModuleWithProviders {
+    /* tslint:disable-next-line */
+    console.warn('forRoot() has been deprecated in CovalentHighlightModule');
     return {
       ngModule: CovalentMarkdownModule,
       providers: [],
