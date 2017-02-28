d5e3b2a6cb26efbf521350464808218a5236262c
emoralesb05 <eduardo.morales@teradata.com>
2017-02-27 21:47:03 -0800

    chore(): deprecate forRoot in chips module

--- src/platform/core/chips/chips.module.ts
+++ src/platform/core/chips/chips.module.ts
@@ -30,7 +30,14 @@ export { TdChipsComponent } from './chips.component';
   ],
 })
 export class CovalentChipsModule {
+  /**
+   * @deprecated in 1.0.0-beta.3
+   *
+   * Please use without calling forRoot()
+   */
   static forRoot(): ModuleWithProviders {
+    /* tslint:disable-next-line */
+    console.warn('forRoot() has been deprecated in CovalentChipsModule');
     return {
       ngModule: CovalentChipsModule,
       providers: [],
