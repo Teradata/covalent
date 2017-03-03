b5ad9cc3e363a1b6545829447af0dbbfa672d2ef
emoralesb05 <eduardo.morales@teradata.com>
2017-03-03 13:59:31 -0800

    chore(): removed themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import { CovalentCoreModule } from '../platform/core';
 import { CovalentHighlightModule } from '../platform/highlight';
 import { CovalentHttpModule } from '../platform/http';
 import { CovalentMarkdownModule } from '../platform/markdown';
-import { CovalentChartsModule } from '@covalent/charts';
 import { CovalentDynamicFormsModule } from '../platform/dynamic-forms';
 
 import { GitHubService } from './services';
@@ -38,7 +37,6 @@ import { getSelectedLanguage, createTranslateLoader } from './utilities/translat
     CovalentHttpModule.forRoot(),
     CovalentHighlightModule.forRoot(),
     CovalentMarkdownModule.forRoot(),
-    CovalentChartsModule.forRoot(),
     CovalentDynamicFormsModule.forRoot(),
     TranslateModule.forRoot({
       loader: {
