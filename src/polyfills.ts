c3f52aed2b7bea8bed3ae5aa6e43f8a5faf65822
Ed Morales <emoralesb05@gmail.com>
2017-04-02 18:04:04 -0700

    feat(rxjs): add individual rxjs imports in needed modules. (#478)
    * chore(rxjs): remove general import from polyfill
    
    we need to get the errors of the individual functions used in our components
    
    * fix(chips): added rxjs timer import
    
    * fix(search): add rxjs skip import
    
    * chore(rollup): update bundle with rxjs imports

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,8 +18,6 @@ import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
-import 'rxjs/Rx';
-
 // If you need to support the browsers/features below, uncomment the import
 // and run `npm install import-name-here';
 // Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
