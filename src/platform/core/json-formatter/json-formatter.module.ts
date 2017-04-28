75e1290ae5f479b59b65e92386cd961840a71585
emoralesb05 <eduardo.morales@teradata.com>
2017-04-28 15:01:33 -0700

    chore(all): remove forRoot() deprecations.
    they were deprecated in beta.3, so we can remove them for beta.4 now

--- src/platform/core/json-formatter/json-formatter.module.ts
+++ src/platform/core/json-formatter/json-formatter.module.ts
@@ -21,15 +21,5 @@ export { TdJsonFormatterComponent } from './json-formatter.component';
   ],
 })
 export class CovalentJsonFormatterModule {
-  /**
-   * @deprecated in 1.0.0-beta.3
-   *
-   * Please use without calling forRoot()
-   */
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: CovalentJsonFormatterModule,
-      providers: [],
-    };
-  }
+
 }
