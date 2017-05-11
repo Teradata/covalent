be7aa2d7be5cb098205ceb6e86eb3b3bc980b00f
emoralesb05 <eduardo.morales@teradata.com>
2017-05-11 16:13:59 -0700

    feat(loading): support for async and boolean with [until] input
    introducing a new syntax to use the tdLoading directive which lets the users leverage the async angular pipe and assign a reference from the result to use within the context.
    
    also leveraging this, we can use booleans to show and hide annonymous loading directives.

--- src/app/components/components/loading/loading.component.ts
+++ src/app/components/components/loading/loading.component.ts
@@ -1,5 +1,8 @@
 import { Component, ViewContainerRef, OnInit, HostBinding, ChangeDetectionStrategy } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
+
 import { slideInDownAnimation } from '../../../app.animations';
 
 import { TdLoadingService, ITdLoadingConfig, LoadingType, LoadingMode } from '../../../../platform/core';
@@ -39,18 +42,30 @@ export class LoadingDemoComponent implements OnInit {
                   Defaults to "primary"`,
     name: 'tdLoadingColor?',
     type: '"primary" | "accent" | "warn"',
+  }, {
+    description: `If its null, undefined or false it will be used to register requests to the mask.
+                  Else if its any value that can be resolved as true, it will resolve the mask.`,
+    name: 'tdLoadingUtil?',
+    type: 'any',
   }];
 
   loadingServiceMethods: Object[] = [{
     description: `Registers a request for the loading mask referenced by the name parameter.
-                  Can optionally pass registers argument to set a number of register calls.`,
+                  Can optionally pass registers argument to set a number of register calls.
+                  If no paramemeters are used, then default main mask will be used.`,
     name: 'register',
     type: 'function(name?: string, registers: number = 1)',
   }, {
     description: `Resolves a request for the loading mask referenced by the name parameter.
-                  Can optionally pass resolves argument to set a number of resolve calls.`,
+                  Can optionally pass resolves argument to set a number of resolve calls.
+                  If no paramemeters are used, then default main mask will be used.`,
     name: 'resolve',
     type: 'function(name?: string, resolves: number = 1)',
+  }, {
+    description: `Resolves all requests for the loading mask referenced by the name parameter.
+                  If no paramemeters are used, then default main mask will be used.`,
+    name: 'resolveAll',
+    type: 'function(name?: string)',
   }, {
     description: `Set value on a loading mask referenced by the name parameter. 
                   Usage only available if its mode is 'determinate'.`,
@@ -63,6 +78,9 @@ export class LoadingDemoComponent implements OnInit {
     type: 'function(options: ITdLoadingConfig)',
   }];
 
+  loading: boolean = false;
+  listObservable: Observable<any[]>;
+
   overlayStarSyntax: boolean = false;
 
   overlayDemo: any = {
@@ -73,6 +91,11 @@ export class LoadingDemoComponent implements OnInit {
   replaceDemo: any = {
     name: '',
     select: '',
+    description: '',
+  };
+
+  untilOverlayDemo: any = {
+    name: '',
     description: '',
   };
 
@@ -130,5 +153,14 @@ export class LoadingDemoComponent implements OnInit {
 
   startDirectives(): void {
     this._loadingService.register('overlayStarSyntax');
+    this.createObservableList();
+  }
+
+  createObservableList(): void {
+    this.listObservable = new Observable<any[]>((subscriber: Subscriber<any[]>) => {
+      setTimeout(() => {
+        subscriber.next([{label: 'Light', value: true}, {label: 'Console', value: false}, {label: 'T.V.', value: true}]);
+      }, 3000);
+    });
   }
 }
