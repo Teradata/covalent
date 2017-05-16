4f84c6ce493996fd749b55d2012f9eb6f4a9e367
Jeremy Smartt <jeremysmartt@users.noreply.github.com>
2017-05-15 21:49:29 -0700

    feat(data-table): onclick event for datatable rows, select event only on checkboxes, multi shift click and basic a11y (closes #468) (closes #592) (#572)
    * feat: Datatable: onclick event for datatable rows
    
    closes #468
    
    * changing event name to ITdDataTableRowClickEvent
    
    * merging develop into branch
    
    * update to have selectable row w/ the checkbox, but only if you click the checkbox element and not the entire row
    
    * adding ability to shift multi select rows in datatable
    
    * updating demo text to say ability to shift click and adding a unit test that simulate the shift click behavior and tests that is works
    
    * chore(): expose ITdDataTableRowClickEvent in the API
    
    * feat(): use DOCUMENT if its there, else dont use it
    
    also point the text selection events directly into the md-pseudo-checkbox instead of the host component and comparing the tagName
    
    * chore(): add (rowClick) in data-table docs
    
    * chore(data-table): remove checked arg in select method
    
    removing it since its not needed anymore in the select method, it wasnt being used.
    
    * chore(data-table): test the click event as output
    
    * feat(data-table): adding keyboard a11y for selections
    
    * feat(data-table): add more keyboard a11y
    
    added space press to select all checkbox
    
    also added support for enter/space/up/down on data table rows
    
    * chore(): simplify shift selection to use index
    
    before it was finding the selected rows, but as a user i would expect to always toggle from first index to last index
    
    also its better to simplify it for performance issues, since with lots of rows this couldve take a toll
    
    * Fixing case where selecting a row, then unselecting the row and then doing shift click.  Also fixing case where selecting a row and then unselecting everything
    
    * Ability for Holding Shift Key and moving up and down the datatable with arrow keys only, no mouse
    
    * clickable input to datatable allows for rowClick event to be fired.  If set to false the event isn't fired.
    
    * chore(): use dialog instead of alert
    
    * fix(): be able to tab when its `selectable`
    
    * fix for when multiple is false and only allowing 1 selection and fix for being able to deselect current selection when multiple is false
    
    * fix unit tests
    
    * chore(): fixed single selection
    
    we need to check the prev state when its single selection

--- src/platform/core/data-table/data-table-row/data-table-row.component.ts
+++ src/platform/core/data-table/data-table-row/data-table-row.component.ts
@@ -11,5 +11,9 @@ export class TdDataTableRowComponent {
   constructor(private _elementRef: ElementRef, private _renderer: Renderer2) {
     this._renderer.addClass(this._elementRef.nativeElement, 'td-data-table-row');
   }
+  
+  focus(): void {
+    this._elementRef.nativeElement.focus();
+  }
 
 }
