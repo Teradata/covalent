9adeeb9e205809b046fbe5b86873f4a02bcbc1a3
emoralesb05 <eduardo.morales@teradata.com>
2017-05-24 11:12:37 -0700

    fix(http): revert to not singleton http service since  devs might want a diff http per module

--- src/platform/http/http.module.ts
+++ src/platform/http/http.module.ts
@@ -8,16 +8,14 @@ export const HTTP_CONFIG: InjectionToken<HttpConfig> = new InjectionToken<HttpCo
 
 export type HttpConfig = {interceptors: IHttpInterceptorConfig[]};
 
-export function HTTP_INTERCEPTOR_PROVIDER_FACTORY(
-  parent: HttpInterceptorService, http: Http, injector: Injector, config: HttpConfig): HttpInterceptorService {
-  return parent || new HttpInterceptorService(http, injector, new URLRegExpInterceptorMatcher(), config.interceptors);
+export function httpFactory(http: Http, injector: Injector, config: HttpConfig): HttpInterceptorService {
+  return new HttpInterceptorService(http, injector, new URLRegExpInterceptorMatcher(), config.interceptors);
 }
 
 export const HTTP_INTERCEPTOR_PROVIDER: Provider = {
-  // If there is already a service available, use that. Otherwise, provide a new one.
   provide: HttpInterceptorService,
-  useFactory: HTTP_INTERCEPTOR_PROVIDER_FACTORY,
-  deps: [[new Optional(), new SkipSelf(), HttpInterceptorService], Http, Injector, HTTP_CONFIG],
+  useFactory: httpFactory,
+  deps: [Http, Injector, HTTP_CONFIG],
 };
 
 @NgModule({
