4260fa10cb4bf4abe2deafb6d42fc8a7080431f4
KL186023 <kyle.ledbetter@teradata.com>
2017-05-29 13:46:47 -0700

    update(components): darker colors

--- src/app/components/components/overview/overview.component.ts
+++ src/app/components/components/overview/overview.component.ts
@@ -14,27 +14,27 @@ export class ComponentsOverviewComponent {
   @HostBinding('class.td-route-animation') classAnimation: boolean = true;
 
   items: Object[] = [{
-      color: 'deep-purple-A400',
+      color: 'deep-purple-A700',
       icon: 'view_list',
       route: 'steps',
       title: 'Stepper',
     }, {
-      color: 'blue-A400',
+      color: 'blue-A700',
       icon: 'open_with',
       route: 'expansion-panel',
       title: 'Expansion Panel',
     }, {
-      color: 'pink-A400',
+      color: 'pink-A700',
       icon: 'space_bar',
       route: 'file-input',
       title: 'File Input',
     }, {
-      color: 'cyan-A400',
+      color: 'cyan-A700',
       icon: 'attach_file',
       route: 'file-upload',
       title: 'File Upload',
     }, {
-      color: 'deep-orange-A400',
+      color: 'deep-orange-A700',
       icon: 'label',
       route: 'chips',
       title: 'Chips',
@@ -44,86 +44,86 @@ export class ComponentsOverviewComponent {
       route: 'loading',
       title: 'Loading',
     }, {
-      color: 'amber-A400',
+      color: 'amber-A700',
       icon: 'open_in_browser',
       route: 'dialogs',
       title: 'Simple Dialogs',
     }, {
-      color: 'green-A400',
+      color: 'green-A700',
       icon: 'grid_on',
       route: 'data-table',
       title: 'Data Table',
     }, {
-      color: 'teal-A400',
+      color: 'teal-A700',
       icon: 'format_indent_increase',
       route: 'json-formatter',
       title: 'JSON Formatter',
     }, {
-      color: 'indigo-A400',
+      color: 'indigo-A700',
       icon: 'swap_horiz',
       route: 'paging',
       title: 'Paging',
     }, {
-      color: 'purple-A400',
+      color: 'purple-A700',
       icon: 'notifications',
       route: 'notifications',
       title: 'Notifications',
     }, {
-      color: 'light-blue-A400',
+      color: 'light-blue-A700',
       icon: 'info_outline',
       route: 'message',
       title: 'Messages',
     }, {
-      color: 'indigo-A400',
+      color: 'indigo-A700',
       icon: 'search',
       route: 'search',
       title: 'Search',
     }, {
-      color: 'red-A400',
+      color: 'red-A700',
       icon: 'devices',
       route: 'media',
       title: 'Media Queries',
     }, {
-      color: 'light-blue-A400',
+      color: 'light-blue-A700',
       icon: 'wb_iridescent',
       route: 'directives',
       title: 'Directives',
     }, {
-      color: 'deep-orange-A400',
+      color: 'deep-orange-A700',
       icon: 'filter_list',
       route: 'pipes',
       title: 'Pipes',
     },
   ];
   optional: Object[] = [{
-      color: 'pink-A400',
+      color: 'pink-A700',
       icon: 'code',
       route: 'syntax-highlighting',
       title: 'Highlighting',
     }, {
-      color: 'orange-A400',
+      color: 'orange-A700',
       icon: 'chrome_reader_mode',
       route: 'markdown',
       title: 'Markdown',
     }, {
-      color: 'green-A400',
+      color: 'green-A700',
       icon: 'format_align_center',
       route: 'dynamic-forms',
       title: 'Dynamic Forms',
     }, {
-      color: 'indigo-A400',
+      color: 'indigo-A700',
       icon: 'http',
       route: 'http',
       title: 'HTTP Service',
     },
   ];
   external: Object[] = [{
-      color: 'purple-A400',
+      color: 'purple-A700',
       icon: 'insert_chart',
       route: 'ngx-charts',
       title: 'NGX-Charts',
     }, {
-      color: 'blue-A400',
+      color: 'blue-A700',
       icon: 'language',
       route: 'ngx-translate',
       title: 'NGX-Translate',
