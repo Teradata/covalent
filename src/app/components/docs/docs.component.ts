90e16f5d7be080aee1601a2d86e72c47536c3e40
Ed Morales <emoralesb05@gmail.com>
2017-06-09 11:18:48 -0700

    feat(dependencies): upgrade to @angular@4.2.0 (#608)
    * feat(toggle): make toggle directive use new animations API
    
    this will grant better support and not depend on `BrowserAnimationsModule` (now we can use NoopAnimationsModule)
    
    * feat(fade): make fade directive use new animations API
    
    this will grant better support and not depend on `BrowserAnimationsModule` (now we can use NoopAnimationsModule)
    
    * fix(media): make media service not use `window` directly
    
    this to start tackling angular universal support
    
    * chore(): bump module dependencies to angular 4.2.0
    
    * chore(): upgrade dependencies and point to 4.2.0-rc.0
    
    pointing to rc.0 to start testing and getting ready for 4.2.0 release
    
    * chore(): polish loading unit tests and use NoopAnimationsModule in all unit test files
    
    * fix(): docs error with change detection init
    
    * fix(dynamic-forms): make max/min not clash with angular ones
    
    also use max/min from angular instead of our implementation
    
    * bump(): to angular/cli@1.1.0
    
    * chore(docs): add td-expansion-panel-group in docs where needed
    
    * chore(): update to stable 4.2.0
    
    * chore(): upgrade yarn.lock
    
    * rollback: fix(): dynamic forms min/max values
    
    * fix(media): media toggle wasnt removing attrs or styles when not matching
    
    * fix(): change detection problem in layout focs
    
    * fix(search-box): error when animating with partial styles
    
    * fix(): animations loading demo
    
    * chore(): bump to ngx-charts@5.3.1 and ngx-translate@7.0.0
    
    * fix(): timeouts no longer needed in loading component
    
    * chore(loading): polish unit tests

--- src/app/components/docs/docs.component.ts
+++ src/app/components/docs/docs.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, AfterViewInit } from '@angular/core';
+import { Component, HostBinding, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { TdMediaService } from '@covalent/core';
 
 import { fadeAnimation } from '../../app.animations';
@@ -66,11 +66,13 @@ export class DocsComponent implements AfterViewInit {
     title: 'Testing',
   }];
 
-  constructor(public media: TdMediaService) {}
+  constructor(private _changeDetectorRef: ChangeDetectorRef,
+              public media: TdMediaService) {}
 
   ngAfterViewInit(): void {
     // broadcast to all listener observables when loading the page
     this.media.broadcast();
+    this._changeDetectorRef.detectChanges();
   }
 
 }
