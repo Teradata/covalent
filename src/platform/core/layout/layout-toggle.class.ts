66345979225ab05f457ef7bb726dfa018946b2f6
emoralesb05 <eduardo.morales@teradata.com>
2017-07-05 17:04:45 -0700

    feat(layout): add toggle/open/close directives for layouts with sidenav.
    so we can open/close/toggle any sidenav from any child component, we need special directives that access their parent layouts.
    
    with this we can affect the behavior of it, and it also includes [hideWhenOpened] input and a default input to disable/enable the action leveraging the media service

--- src/platform/core/layout/layout-toggle.class.ts
+++ src/platform/core/layout/layout-toggle.class.ts
@@ -1,33 +1,52 @@
-import { Input, HostBinding, HostListener, Renderer2, ElementRef } from '@angular/core';
+import { Input, HostBinding, HostListener, Renderer2, ElementRef, AfterViewInit } from '@angular/core';
 
-import { MdSidenavToggleResult } from '@angular/material';
+import { MdSidenavToggleResult, MdSidenav } from '@angular/material';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { merge } from 'rxjs/observable/merge';
 
 export interface ILayoutTogglable {
-  opened: boolean;
+  sidenav: MdSidenav;
   toggle(): Promise<MdSidenavToggleResult>;
+  open(): Promise<MdSidenavToggleResult>;
+  close(): Promise<MdSidenavToggleResult>;
 }
 
-export abstract class LayoutToggle {
+export abstract class LayoutToggle implements AfterViewInit {
+
+  private _initialized: boolean = false;
+  private _disabled: boolean = false;
+  private _hideWhenOpened: boolean = false;
+
+  set disabled(disabled: boolean) {
+    this._disabled = disabled;
+  }
 
   /**
    * hideWhenOpened?: boolean
-   * When this is set to false, the host will not be hidden when
-   * the layout is set to [opened]="true".
+   * When this is set to true, the host will be hidden when
+   * the sidenav is opened.
    */
-  @Input('hideWhenOpened') hideWhenOpened: boolean = true;
-
-  /**
-   * Hides element if layout is opened and [hideWhenOpened] is set to true
-   */
-  @HostBinding('style.display')
-  get hiddenBinding(): string {
-    return this._layout.opened && this.hideWhenOpened ? 'none' : undefined;
+  @Input('hideWhenOpened') 
+  set hideWhenOpened(hideWhenOpened: boolean) {
+    this._hideWhenOpened = hideWhenOpened;
+    if (this._initialized) {
+      this._toggleVisibility();
+    }
   }
 
-  constructor(private _layout: ILayoutTogglable,
+  constructor(protected _layout: ILayoutTogglable,
               private _renderer: Renderer2,
               private _elementRef: ElementRef) {
     this._renderer.addClass(this._elementRef.nativeElement, 'td-layout-menu-button');
+  }
+
+  ngAfterViewInit(): void {
+    this._initialized = true;
+    merge(this._layout.sidenav.onOpenStart, this._layout.sidenav.onCloseStart).subscribe(() => {
+      this._toggleVisibility();
+    });
   }
 
   /**
@@ -36,7 +55,19 @@ export abstract class LayoutToggle {
   @HostListener('click', ['$event'])
   clickListener(event: Event): void {
     event.preventDefault();
-    this._layout.toggle();
+    if (!this._disabled) {
+      this.onClick();
+    }
+  }
+
+  abstract onClick(): void;
+
+  private _toggleVisibility(): void {
+    if (this._layout.sidenav._opened && this._hideWhenOpened) {
+      this._renderer.setStyle(this._elementRef.nativeElement, 'display', 'none');
+    } else {
+      this._renderer.setStyle(this._elementRef.nativeElement, 'display', '');
+    }
   }
 
 }
