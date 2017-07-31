7a53443fba7e83e72e94098e004efd2fc8df8c66
Steven Ov <Steven.Ov@Teradata.com>
2017-07-31 11:18:22 -0700

    refactor(): Added comments and removed material example

--- src/universal-app/combat-training/combat-training.ts
+++ src/universal-app/combat-training/combat-training.ts
@@ -1,9 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { BrowserModule } from '@angular/platform-browser';
-// import { MdButtonModule } from '@angular/material';
-import { CovalentPagingModule } from '@covalent/core';
+import { CovalentMessageModule } from '@covalent/core';
 
+/**
+ * This component is used to test whether or if the supplied Covalent components can be
+ * rendered server side. If not then a diagnostic error will let us know what needs to be
+ * fixed inorder to make them server side compatible.
+ *
+ * Use `npm run combat-training` to run the server side rendering diagnostic script.
+ */
 @Component({
   selector: 'combat-training',
   templateUrl: './combat-training.html',
@@ -11,17 +17,23 @@ import { CovalentPagingModule } from '@covalent/core';
 })
 export class CombatTrainingComponent { }
 
+/**
+ * Client side module
+ */
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({ appId: 'combat-training' }),
-    // MdButtonModule,
-    CovalentPagingModule,
+    CovalentMessageModule,
+    // Add additional Covlent components here and within the template
   ],
   bootstrap: [CombatTrainingComponent],
   declarations: [CombatTrainingComponent],
 })
 export class CombatTrainingClientModule { }
 
+/**
+ * Server side module
+ */
 @NgModule({
   imports: [CombatTrainingClientModule, ServerModule],
   bootstrap: [CombatTrainingComponent],
