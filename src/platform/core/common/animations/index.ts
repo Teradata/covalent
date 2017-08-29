b02c462fc96d6c515289fe69c48e1482d93eee2c
stevenov7 <stevenov7@users.noreply.github.com>
2017-08-29 09:46:16 -0700

    refactor(): move animation exports to common.module.ts

--- src/platform/core/common/animations/index.ts
+++
@@ -1,3 +0,0 @@
-export { TdRotateAnimation, IRotateAnimation } from './rotate/rotate.animation';
-export { TdCollapseAnimation } from './collapse/collapse.animation';
-export { TdFadeInOutAnimation } from './fade/fadeInOut.animation';
