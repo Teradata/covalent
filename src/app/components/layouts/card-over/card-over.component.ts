1e0affc9956f292c4ca3a2d0521c6b44db70f6e2
emoralesb05 <eduardo.morales@teradata.com>
2017-12-07 13:42:46 -0800

    fix(): remove fade route animations between main links

--- src/app/components/layouts/card-over/card-over.component.ts
+++ src/app/components/layouts/card-over/card-over.component.ts
@@ -1,18 +1,12 @@
 import { Component, HostBinding, ChangeDetectionStrategy } from '@angular/core';
 
-import { fadeAnimation } from '../../../app.animations';
-
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'layouts-card-over',
   styleUrls: ['./card-over.component.scss'],
   templateUrl: './card-over.component.html',
-  animations: [fadeAnimation],
 })
 export class CardOverComponent {
-
-  @HostBinding('@routeAnimation') routeAnimation: boolean = true;
-  @HostBinding('class.td-route-animation') classAnimation: boolean = true;
 
   routes: Object[] = [{
       icon: 'home',
--- src/app/components/layouts/layouts.component.ts
+++ src/app/components/layouts/layouts.component.ts
@@ -1,16 +1,10 @@
 import { Component, HostBinding } from '@angular/core';
 
-import { fadeAnimation } from '../../app.animations';
-
 @Component({
   selector: 'app-layouts',
   styleUrls: ['./layouts.component.scss'],
   templateUrl: './layouts.component.html',
-  animations: [fadeAnimation],
 })
 export class LayoutsComponent {
-
-  @HostBinding('@routeAnimation') routeAnimation: boolean = true;
-  @HostBinding('class.td-route-animation') classAnimation: boolean = true;
 
 }
