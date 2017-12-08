a0359466db29775e0714c208c53f8058a17fff52
Ed Morales <emoralesb05@gmail.com>
2017-12-07 16:17:28 -0800

    chore(): upgrade to angular@5.1, material@5 and cli@1.6 (#1013)
    * chore(): upgrade to angular@5.1, material@5 and cli@1.6
    
    * chore(typography): fix font weight for buttons & toolbars
    
    * chore(): remove margin from notifications menu
    
    * fix(): fix layout issue with sidenav
    
    * fix(): remove fade route animations between main links
    
    * chore(): update issue template and add `npm run serve`
    
    * feat(docs): updated breakpoints & grid list
    
    * feat(docs): make overview gridlist responsive
    
    * feat(docs): add ripple to grid tile & larger icon

--- src/app/components/layouts/manage-list/manage-list.component.ts
+++ src/app/components/layouts/manage-list/manage-list.component.ts
@@ -1,19 +1,13 @@
 import { Component, HostBinding, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { TdMediaService } from '@covalent/core';
 
-import { fadeAnimation } from '../../../app.animations';
-
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'layouts-manage-list',
   styleUrls: ['./manage-list.component.scss'],
   templateUrl: './manage-list.component.html',
-  animations: [fadeAnimation],
 })
 export class ManageListComponent {
-
-  @HostBinding('@routeAnimation') routeAnimation: boolean = true;
-  @HostBinding('class.td-route-animation') classAnimation: boolean = true;
 
   routes: Object[] = [{
       icon: 'home',
