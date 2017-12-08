94a9c64c8156475af174f9e0c6ea6edf1b51b077
Ed Morales <emoralesb05@gmail.com>
2017-12-07 16:38:39 -0800

    fix(animations): allow child animations by default in our pre-canned animations (#1016)

--- src/platform/core/common/animations/jello/jello.animation.ts
+++ src/platform/core/common/animations/jello/jello.animation.ts
@@ -1,5 +1,6 @@
 
-import { trigger, state, style, keyframes, transition, animate, AnimationTriggerMetadata, AUTO_STYLE } from '@angular/animations';
+import { trigger, state, style, keyframes, transition, animate,
+         AnimationTriggerMetadata, AUTO_STYLE, query, animateChild, group } from '@angular/animations';
 import { IAnimationOptions } from '../common/interfaces';
 
 /**
@@ -21,16 +22,21 @@ export function TdJelloAnimation(jelloOptions: IAnimationOptions = {}): Animatio
     state('1',  style({
       transform: 'none',
     })),
-    transition('0 <=> 1', animate((jelloOptions.duration || 500) + 'ms ' + (jelloOptions.delay || 0) + 'ms', keyframes([
-        style({transform: 'none', offset: 0}),
-        style({transform: 'none', offset: 0.011}),
-        style({transform: 'skewX(-12.5deg) skewY(-12.5deg)', offset: 0.222}),
-        style({transform: 'skewX(6.25deg) skewY(6.25deg)', offset: 0.333}),
-        style({transform: 'skewX(-3.125deg) skewY(-3.125deg)', offset: 0.444}),
-        style({transform: 'skewX(1.5625deg) skewY(1.5625deg)', offset: 0.555}),
-        style({transform: 'skewX(-0.78125deg) skewY(-0.78125deg)', offset: 0.666}),
-        style({transform: 'skewX(0.390625deg) skewY(0.390625deg)', offset: 0.777}),
-        style({transform: 'skewX(-0.1953125deg) skewY(-0.1953125deg)', offset: 0.888}),
-    ]))),
+    transition('0 <=> 1', [
+      group([
+        query('@*', animateChild(), { optional: true }),
+        animate((jelloOptions.duration || 500) + 'ms ' + (jelloOptions.delay || 0) + 'ms', keyframes([
+          style({transform: 'none', offset: 0}),
+          style({transform: 'none', offset: 0.011}),
+          style({transform: 'skewX(-12.5deg) skewY(-12.5deg)', offset: 0.222}),
+          style({transform: 'skewX(6.25deg) skewY(6.25deg)', offset: 0.333}),
+          style({transform: 'skewX(-3.125deg) skewY(-3.125deg)', offset: 0.444}),
+          style({transform: 'skewX(1.5625deg) skewY(1.5625deg)', offset: 0.555}),
+          style({transform: 'skewX(-0.78125deg) skewY(-0.78125deg)', offset: 0.666}),
+          style({transform: 'skewX(0.390625deg) skewY(0.390625deg)', offset: 0.777}),
+          style({transform: 'skewX(-0.1953125deg) skewY(-0.1953125deg)', offset: 0.888}),
+        ])),
+      ]),
+    ]),
   ]);
 }
