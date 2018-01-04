997cadc6844960027fee332417dc6e7e48129d90
Steven Ov <Steven.Ov@Teradata.com>
2018-01-03 16:08:24 -0800

    feat(): cherry-pick over inital ssr research/solution to new clean branch last 2 branches were outdated and getting too dirty

---
+++ scripts/so-custom-compile-sass.js
@@ -0,0 +1,261 @@
+'use strict';
+
+var gulp = require('gulp-help')(require('gulp'));
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var config = require('../build.conf');
+var sassImporter = require('./sass-importer');
+
+gulp.task('so-compile-sass', 'Build the module styles', function() {
+  return gulp
+    .src(config.paths.styles)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('deploy/'));
+});
+
+gulp.task('compile-paging-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/paging/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/paging'));
+});
+
+gulp.task('compile-virtual-scroll-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/virtual-scroll/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/virtual-scroll'));
+});
+
+gulp.task('compile-notifications-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/notifications/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/notifications'));
+});
+
+gulp.task('compile-common-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/common/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/common'));
+});
+
+gulp.task('compile-message-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/message/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/message'));
+});
+
+gulp.task('compile-chips-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/chips/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/chips'));
+});
+
+gulp.task('compile-data-table-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/data-table/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/data-table'));
+});
+
+gulp.task('compile-dialogs-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/dialogs/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/dialogs'));
+});
+
+gulp.task('compile-expansion-panel-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/expansion-panel/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/expansion-panel'));
+});
+
+gulp.task('compile-file-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/file/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/file'));
+});
+
+gulp.task('compile-json-formatter-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/json-formatter/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/json-formatter'));
+});
+
+gulp.task('compile-layout-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/layout/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/layout'));
+});
+
+gulp.task('compile-loading-sass', 'Build the module styles', function() {
+  return gulp
+    .src(['src/platform/core/loading/**/**.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      importer: sassImporter,
+    }))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'last 2 versions',
+          'ie 11'
+        ]
+      })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/packages/core/loading'));
+});
--- src/universal-app/battle-royale.ts
+++ src/universal-app/battle-royale.ts
@@ -1,15 +1,16 @@
 import 'reflect-metadata';
-import 'zone.js/dist/zone-node.js';
+import 'zone.js';
 import { enableProdMode } from '@angular/core';
 import { renderModuleFactory } from '@angular/platform-server';
+import {join} from 'path';
 
-import { CombatTrainingServerModuleNgFactory } from './dist/aot/combat-training/combat-training.ngfactory';
+import { CombatTrainingServerModuleNgFactory } from './combat-training/combat-training.ngfactory';
 import { readFileSync } from 'fs-extra';
 
 enableProdMode();
 
 const result: Promise<any> = renderModuleFactory(CombatTrainingServerModuleNgFactory, {
-  document: readFileSync('src/universal-app/index.html', 'utf-8'),
+  document: readFileSync(join(__dirname, 'index.html'), 'utf-8'),
 });
 
 result.then((html: string) => process.stdout.write(html));
