479737ec902d3ec6402a7deb85dad1caa735ef83
emoralesb05 <eduardo.morales@teradata.com>
2018-01-04 14:04:33 -0800

    chore(): combine everything into a file

--- scripts/ci/travis-script.sh
+++ scripts/ci/travis-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Script that runs in every Travis CI container.
+# Responsible for testing the project in different Travis jobs of the current build stage.
 
 # The script should immediately exit if any command in the script fails.
 set -e
@@ -13,4 +14,34 @@ if [[ -z "$TRAVIS" ]]; then
   exit 1
 fi
 
-./scripts/ci/travis-testing.sh
+echo ""
+echo "Building sources and running tests. Running mode: ${MODE}"
+echo ""
+
+# Get commit diff
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  fileDiff=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
+else
+  fileDiff=$(git diff --name-only $TRAVIS_BRANCH...HEAD)
+fi
+
+# Check if tests can be skipped
+if [[ ${fileDiff} =~ ^(.*\.md\s*)*$ ]]; then
+  echo "Skipping tests since only markdown files changed."
+  exit 0
+fi
+
+if "${MODE}" = lint; then
+  npm run lint
+elif "${MODE}" = aot; then
+  npm run build:docs
+elif "${MODE}" = release; then
+  npm run build:release
+elif "${MODE}" = unit; then
+  npm run test
+fi
+
+# Upload coverage results if those are present.
+if [ -f coverage/lcov.info ]; then
+  npm run coveralls
+fi
