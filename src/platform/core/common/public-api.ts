eb5bdf32ef37b0ee3985621721106c25078901e9
Steven Ov <Steven.Ov@Teradata.com>
2018-01-09 12:55:49 -0800

    fix(): remove message export from message.module and update common public_api

--- src/platform/core/common/public-api.ts
+++ src/platform/core/common/public-api.ts
@@ -1,39 +1,39 @@
 export * from './common.module';
 
 // Directives
-export { TdToggleDirective } from './animations/toggle/toggle.directive';
-export { TdFadeDirective } from './animations/fade/fade.directive';
+export * from './animations/toggle/toggle.directive';
+export * from './animations/fade/fade.directive';
 
 // Utility functions
-export { TdRotateAnimation, IRotateAnimation } from './animations/rotate/rotate.animation';
-export { TdCollapseAnimation, ICollapseAnimation } from './animations/collapse/collapse.animation';
-export { TdFadeInOutAnimation, IFadeInOutAnimation } from './animations/fade/fadeInOut.animation';
-export { TdBounceAnimation } from './animations/bounce/bounce.animation';
-export { TdFlashAnimation } from './animations/flash/flash.animation';
-export { TdHeadshakeAnimation } from './animations/headshake/headshake.animation';
-export { TdJelloAnimation } from './animations/jello/jello.animation';
-export { TdPulseAnimation } from './animations/pulse/pulse.animation';
+export * from './animations/rotate/rotate.animation';
+export * from './animations/collapse/collapse.animation';
+export * from './animations/fade/fadeInOut.animation';
+export * from './animations/bounce/bounce.animation';
+export * from './animations/flash/flash.animation';
+export * from './animations/headshake/headshake.animation';
+export * from './animations/jello/jello.animation';
+export * from './animations/pulse/pulse.animation';
 
 /**
  * BEHAVIORS
  */
-export { IControlValueAccessor, mixinControlValueAccessor } from './behaviors/control-value-accesor.mixin';
-export { ICanDisable, mixinDisabled } from './behaviors/disabled.mixin';
-export { ICanDisableRipple, mixinDisableRipple } from './behaviors/disable-ripple.mixin';
+export * from './behaviors/control-value-accesor.mixin';
+export * from './behaviors/disabled.mixin';
+export * from './behaviors/disable-ripple.mixin';
 
 /**
  * FORMS
  */
 // Form Directives
-export { TdAutoTrimDirective } from './forms/auto-trim/auto-trim.directive';
+export * from './forms/auto-trim/auto-trim.directive';
 // Validators
-export { CovalentValidators } from './forms/validators/validators';
+export * from './forms/validators/validators';
 
 /**
  * PIPES
  */
-export { TdTimeAgoPipe } from './pipes/time-ago/time-ago.pipe';
-export { TdTimeDifferencePipe } from './pipes/time-difference/time-difference.pipe';
-export { TdBytesPipe } from './pipes/bytes/bytes.pipe';
-export { TdDigitsPipe } from './pipes/digits/digits.pipe';
-export { TdTruncatePipe } from './pipes/truncate/truncate.pipe';
+export * from './pipes/time-ago/time-ago.pipe';
+export * from './pipes/time-difference/time-difference.pipe';
+export * from './pipes/bytes/bytes.pipe';
+export * from './pipes/digits/digits.pipe';
+export * from './pipes/truncate/truncate.pipe';
