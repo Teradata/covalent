9ce233aa41fa1a6f9cd699fa11d385dc282c0a0e
Ilsun Park <ilsun.park@gmail.com>
2018-01-18 12:32:06 -0600

    chore(deps): update to node 8.9.4 + pa11y support and CI test (#1072)
    * chore(dependencies): update for node 8 deprecations
    
    * chore(travis) update .travis.yml
    
    * feat(a11y) add pa11y support
    
    * chore(build): update travis.yml
    
    * chore(install): add install to a11y script
    
    * chore(): removing install in script since Travis does this and caches node_modules
    
    this is done to speed up the CI pipeline

---
+++ scripts/a11y/a11y.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Set a number of loops before timing out.
+LIMIT=12
+
+# Time to wait before re-checking
+SLEEP=10
+
+# Initialize the timeout variable
+timeout=0
+
+# Function to wait for ng serve to finish.
+wait_for() {
+    until curl -sf "$1"
+    do
+      if [ $timeout -eq $LIMIT ]; then
+      echo -e "\nBuild timed out.\n"
+        kill $NPM_PID
+        exit 1
+      fi
+      echo -e "\nWaiting for $1...\n"
+      sleep $SLEEP
+      timeout=$((timeout + 1))
+    done
+    echo ""
+}
+
+# Reset in case getopts has been used previously in the shell.
+OPTIND=1
+
+# Initialize our own variables:
+routes="routes.txt"
+server="localhost:4200"
+
+# Get command line args
+while getopts "r:s:" opt; do
+  case "$opt" in
+  r)  routes=$OPTARG
+    ;;
+  s)  server=$OPTARG
+    ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+# Run ng serve in the background and save the process ID.
+npm run serve &
+NPM_PID=$!
+
+# wait for the server to return.
+wait_for $server
+
+# iterate over routes and pa11y them.
+for route in $(cat ${routes});
+do
+  route=$(echo $route|tr -d '\n')
+  ./node_modules/.bin/pa11y ${server}/\#/${route};
+done
+
+# stop the ng server process.
+kill $NPM_PID
