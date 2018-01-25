eb72cc41dbb8620d6a95a29875cb9d2dbfaeee1b
Jenn Medellin <54321jenn@gmail.com>
2018-01-25 15:11:20 -0800

    chore(): update patterns to use stackblitz (#1064)
    * chore()/update cards pattern
    
    - Add stackblitz example
    - Modify layout to match new patterns
    
    * chore()/update patterns
    
    - Remove example logic from cards
    - Rework alert pattern
    
    * chore()/update mgmt list
    
    - update management list example to stackblitz
    - fix layout
    
    * chore()/update mgmt list
    
    - update ts
    
    * chore()/update patterns
    
    - update mgmt list & nav drawer pattern layouts & examples
    
    * chore()/update layouts
    
    - cleanup all layouts
    
    * chore()/clean up
    
    - Clean up guidance
    
    * chore(docs): update patterns
    
    - Add [inset]="true" on MatDividers
    - Fix Do's and Don'ts (lost in code conflict)
    
    * chore(docs)/update patterns to stackblitz
    
    - make all typography divs include relative class
    
    * chore(docs)/update to stackblitz
    
    - remove unused css
    
    * chore(patterns): fix dividers & lists
    
    * chore(theme): fix link style
    
    * chore(patterns): add missing dividers

--- src/app/components/design-patterns/cards/cards.component.ts
+++ src/app/components/design-patterns/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostBinding } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { TdCollapseAnimation } from '@covalent/core/common';
 import { slideInDownAnimation } from '../../../app.animations';
@@ -9,33 +10,16 @@ import { slideInDownAnimation } from '../../../app.animations';
   templateUrl: './cards.component.html',
   animations: [
     slideInDownAnimation,
-    TdCollapseAnimation(),
   ],
 })
 export class CardsComponent {
 
   @HostBinding('@routeAnimation') routeAnimation: boolean = true;
   @HostBinding('class.td-route-animation') classAnimation: boolean = true;
-
-  username: string;
-  password: string;
-  fadeDiv: boolean = true;
-  example1: boolean = true;
-  example2: boolean = true;
-  example3: boolean = true;
-  example4: boolean = true;
-  example5: boolean = true;
   
-  invalidError: boolean = false;
-
-  login(): void {
-    this.invalidError = true;
-    setTimeout(() => {
-      this.invalidError = false;
-    }, 500);
-  }
+  source: any = '';
   
-  toggle(div: string): void {
-    this[div] = !this[div];
+  constructor(private sanitizer: DomSanitizer) {
+    this.source = sanitizer.bypassSecurityTrustResourceUrl('https://cards-patterns.stackblitz.io/');
   }
 }
