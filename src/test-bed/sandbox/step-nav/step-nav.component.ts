23b2a11090abc8576a1386fe837d0e797a1ccab5
richa <richa.vyas@teradata.com>
2018-07-27 13:09:31 -0700

    Nav Stepper included in the demo section

---
+++ src/test-bed/sandbox/step-nav/step-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'step-nav-demo',
+  styleUrls: [ './step-nav.component.scss' ],
+  templateUrl: './step-nav.component.html',
+})
+export class StepNavDemoComponent {
+
+}
