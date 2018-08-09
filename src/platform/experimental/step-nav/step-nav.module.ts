33acdafdeee59f671aa35c3cdc4a1452cc7f129a
richa <richa.vyas@teradata.com>
2018-08-09 14:59:36 -0700

    changes in moduling

--- src/platform/experimental/step-nav/step-nav.module.ts
+++ src/platform/experimental/step-nav/step-nav.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
 
 import { TdStepNavComponent } from './step-nav.component';
-import { TdStepLinkComponent } from './step-link/step-link.component';
+import { TdStepLinkComponent, TdStepLinkLabelDirective } from './step-link/step-link.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatIconModule,
+    MatRippleModule,
   ],
   declarations: [
     TdStepNavComponent,
     TdStepLinkComponent,
+    TdStepLinkLabelDirective,
   ],
   exports: [
     TdStepNavComponent,
     TdStepLinkComponent,
+    TdStepLinkLabelDirective,
   ],
 })
 export class CovalentStepNavModule {
