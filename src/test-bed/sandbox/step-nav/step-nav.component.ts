7e3105f21116e82ea9f8587043c3748f397791fd
richa <richa.vyas@teradata.com>
2018-08-09 14:55:32 -0700

    change the way testbed would render step-nav

--- src/test-bed/sandbox/step-nav/step-nav.component.ts
+++ src/test-bed/sandbox/step-nav/step-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StepLinkState } from '../../../platform/experimental/step-nav/step-link/step-link.component';
 
 @Component({
   selector: 'step-nav-demo',
@@ -7,8 +8,8 @@ import { Component } from '@angular/core';
 })
 export class StepNavDemoComponent {
   routeLinks = [
-    {label: 'Home', link: '/', disabled: true},
-    {label: 'Breadcrumb', link: '../breadcrumbs', disabled: false},
-    {label: 'Tab Select', link: '../tabselect', disabled: false}];
+    {label: 'Home', sublabel: 'Home to navigate to all experimental components', link: ['../'], disabled: false},
+    {label: 'Breadcrumb', sublabel: 'Breadcrumbs for navigation', link: ['../breadcrumbs'], disabled: false, state: StepLinkState.Required},
+    {label: 'Tab Select', sublabel: 'Tab group component that behaves like a mat-select', link: ['../tabselect'], disabled: true, state: StepLinkState.Complete}];
   activeLink = this.routeLinks[0];
 }
