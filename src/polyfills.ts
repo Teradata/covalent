a9a067de3e349220284dd336814268eb66922409
Steven Ov <stevenov7@users.noreply.github.com>
2018-10-25 10:34:20 -0700

    feat(upgrade): upgrade to us angular 7 rc.0/1 (#1252)
    * feat(): upgrade to max cap angular 6.x.x at time of writing
    
    Upgrade steps used below:
    1) ng update @angular/cli --from=6.0.1
    2) ng update rxjs --from=6.3.3
    3) yarn upgrade tsickle@0.32.1
       - (b/c core and material upgrade (below) require typescript 2.9.2 support - ng update was complaining - 0.33.0 support typescript 3)
    4) ng update @angular/core --from=6.0.2
    5) remove unused tests helpers causing typescript tying complaints
    6) ng update @angular/material --from=6.0.2
    7) ng update @angular-devkit/build-angular
    
    * feat(): upgrade to angular 7.0.0-rc.0
    
    steps used below:
    1) ng update @angular/cli --next --from=6.2.4
    2) yarn upgrade tsickle@0.33.0
    3) ng update @angular/core --next --force --from=6.1.9
    4) ng update @angular/cdk --next --force --from=6.4.7
    5) ng update @angular/material --next --force --from=6.4.7
    6) yarn upgrade codelyzer@4.5.0
    7) yarn upgrade ng-packagr@4.3.0
    
    * chore(): update other dependencies
    
    * fix(): scroll issue in management/nav-list layouts
    
    * fix(): layout dom change for scrolling
    
    * fix(layout): safari collapse animation issue
    
    * feat(collapse): consistency change display to visibility auto style
    
    * fix(animation): fade/in/out on safari not working
    
    * fix(animation): similar safari fixes for depercated collapse animation
    
    * fix(scss): elevation function signature changes
    
    * feat(universal): upgrade universal app to use angular 7 rc versions
    
    * fix(typing): fix upgraded type being used/returned
    
    * chore(): update to 7.0.0 release + fixes
    
    * fix(): lint error
    
    * fix(): lint issue
    
    * chore(): fix angular.json to be able to run unit tests
    
    * fix(steps): unit tests
    
    * fix(loading): unit tests
    
    * fix(json-formatter): unit tests
    
    * fix(expansion-panel): unit tests
    
    * fix(docs): funky scrollbar in virtual scroll docs when always visible
    
    * fix(): remove border sidenav from layout-nav-list
    
    * fix(): logo fix in markdown
    
    * fix(logo): broken navigation to navigation-drawer
    
    * fix(): reduce z-index on branding top to 999
    
    * fix(layout-nav): right to left border on sidenav

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -16,7 +16,6 @@ import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 
-import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
 // If you need to support the browsers/features below, uncomment the import
