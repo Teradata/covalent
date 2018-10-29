cf6e57be6dea6f18b37a8afbb1120b6563195b55
emoralesb05 <eduardo.morales@teradata.com>
2018-10-29 14:49:13 -0700

    chore(): remove unneeded build.conf.js options

--- build.conf.js
+++ build.conf.js
@@ -3,32 +3,5 @@
 module.exports = {
   deployed: 'deploy/platform/',
   angularVersion: '7.0.0',
-  materialVersion: '7.0.0',
-  paths: {
-    PostNgPackngrBuildRequiredFiles: [
-      '!src/platform/core/**/*.component.scss',
-      'src/platform/core/**/*.scss',
-
-      'src/platform/core/**/codepoints',
-      'src/platform/core/**/MaterialIcons-Regular.eot',
-      'src/platform/core/**/MaterialIcons-Regular.ijmap',
-      'src/platform/core/**/MaterialIcons-Regular.ttf',
-      'src/platform/core/**/MaterialIcons-Regular.woff',
-      'src/platform/core/**/MaterialIcons-Regular.woff2',
-      'src/platform/core/**/*.md',
-    ],
-    PostNgPackngrCompileStyles: [
-      'deploy/platform/core/**/material-icons.scss',
-      'deploy/platform/core/**/platform.scss',
-      'deploy/platform/core/**/theming/prebuilt/**/*.scss'
-    ],
-    PostNgPackngrAdditionalFiles: [
-      '!src/platform/core/**/*.component.scss',
-      '!src/platform/core/**/*.scss',
-      '!src/platform/**/*.component.scss',
-      'src/platform/**/*.scss',
-      '!src/platform/core/**/*.md',
-      'src/platform/**/*.md',
-    ],
-  }
+  materialVersion: '7.0.0'
 };
