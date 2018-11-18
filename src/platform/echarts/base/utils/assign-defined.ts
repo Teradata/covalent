e05ccb1d5f7d1e5f9a2ee9ac8d797f6cfdd0005e
emoralesb05 <eduardo.morales@teradata.com>
2018-11-18 12:09:30 -0800

    fix(utils): remove keys from target that are not in any source

--- src/platform/echarts/base/utils/assign-defined.ts
+++ src/platform/echarts/base/utils/assign-defined.ts
@@ -1,6 +1,8 @@
 export function assignDefined(target: any, ...sources: any[]): any {
+  let keys: Set<string> = new Set<string>();
   for (const source of sources) {
     for (const key of Object.keys(source)) {
+      keys.add(key);
       const val: any = source[key];
       /* tslint:disable-next-line */
       if (val !== undefined && val !== null) {
@@ -9,6 +11,12 @@ export function assignDefined(target: any, ...sources: any[]): any {
       } else if (val === null) {
         delete target[key];
       }
+    }
+  }
+  // delete keys that are not in any source
+  for (const key of Object.keys(target)) {
+    if (!keys.has(key)) {
+      delete target[key];
     }
   }
   return target;
