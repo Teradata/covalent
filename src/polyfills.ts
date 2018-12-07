20d069491145249d931d61b4594b50b58eb874cd
stevenov7 <stevenov7@users.noreply.github.com>
2018-12-07 14:31:02 -0800

    feat(): upgrade to latest Angular and Monaco

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -15,7 +15,7 @@ import 'core-js/es6/map';
 import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 
-import 'core-js/es7/reflect';
+
 import 'zone.js/dist/zone';
 
 // If you need to support the browsers/features below, uncomment the import
