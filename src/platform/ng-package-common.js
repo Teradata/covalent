683736f420e4751d56994fdb511d664318aad9eb
Steven Ov <Steven.Ov@Teradata.com>
2018-12-06 17:19:04 -0800

    feat(): upgrade/support Angular 7.1.2

--- src/platform/ng-package-common.js
+++ src/platform/ng-package-common.js
@@ -2,8 +2,11 @@ module.exports =
 {
   "lib": {
     "entryFile": "index.ts",
+
     "umdModuleIds": {
-      "@covalent/text-editor": "covalent.text-editor"
+      "@covalent/text-editor": "covalent.text-editor",
+      "simplemde": "SimpleMDE",
+      "marked": "marked"
     }
   },
   "whitelistedNonPeerDependencies": ["."]
