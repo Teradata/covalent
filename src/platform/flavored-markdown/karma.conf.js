a81624696de21feffe1ebdeaac1c12400e9f515f
Christian Memije <christianmemije@gmail.com>
2018-12-07 12:15:30 -0800

    refactor(@covalent/flavored-markdown): Replace ng-packagr with cli

---
+++ src/platform/flavored-markdown/karma.conf.js
@@ -0,0 +1,4 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+const defaultConfig = require('../../karma-config-default');
+module.exports = defaultConfig;
