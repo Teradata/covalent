10fed834d5b15d2e8ee7a93cc771941403bdb504
emoralesb05 <eduardo.morales@teradata.com>
2018-12-09 18:26:24 -0800

    fix(http): remove aot warning compilation

--- src/platform/http/public-api.ts
+++ src/platform/http/public-api.ts
@@ -1,6 +1,6 @@
 export * from './http.module';
 export * from './interceptors/url-regexp-interceptor-matcher.class';
-export * from './interceptors/http.service';
+export { ITdHttpInterceptorConfig, TdHttpService } from './interceptors/http.service';
 export * from './interceptors/http-interceptor.interface';
 export * from './interceptors/http-interceptor-matcher.interface';
 export * from './interceptors/http-interceptor-mapping.interface';
