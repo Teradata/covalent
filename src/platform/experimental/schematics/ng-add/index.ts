7e3685658d3176c097e196f5bc6d46da53d73db1
richa <richa.vyas@teradata.com>
2019-01-21 22:41:52 -0800

    Update test

--- src/platform/experimental/schematics/ng-add/index.ts
+++ src/platform/experimental/schematics/ng-add/index.ts
@@ -1,5 +1,4 @@
 import { Rule, chain, Tree, apply, mergeWith, url, FileVisitor, FilePredicate } from '@angular-devkit/schematics';
-import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schematics/tasks';
 import { addPackageToPackageJson } from './package-config';
 import { ISchema } from './schema';
 import { covalentCoreVersion } from './version-names';
@@ -7,7 +6,6 @@ import { covalentCoreVersion } from './version-names';
 export function addCoreDepsAndFiles(options: ISchema): Rule {
   return chain([
     (host: Tree) => addPackageToPackageJson(host, '@covalent/core', `~${covalentCoreVersion}`),
-    (host: Tree) => {host.visit((filePath: any) => console.log(filePath)); return host; },
     mergeWith(url('./files')),
   ]);
 }
