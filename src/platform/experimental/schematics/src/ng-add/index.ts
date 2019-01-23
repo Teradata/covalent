69eb00065aa33cfa39989acff61e0db89555a1a1
richa <richa.vyas@teradata.com>
2019-01-22 17:26:41 -0800

    Add additional Covalent modules

--- src/platform/experimental/schematics/src/ng-add/index.ts
+++ src/platform/experimental/schematics/src/ng-add/index.ts
@@ -1,11 +1,19 @@
 import { Rule, chain, Tree, apply, mergeWith, url, FileVisitor, FilePredicate } from '@angular-devkit/schematics';
 import { addPackageToPackageJson } from './package-config';
 import { ISchema } from './schema';
-import { covalentCoreVersion } from './version-names';
+import { covalentCoreVersion, materialVersion } from './version-names';
 
-export function addCoreDepsAndFiles(options: ISchema): Rule {
+export function addDepsAndFiles(options: ISchema): Rule {
   return chain([
-    (host: Tree) => addPackageToPackageJson(host, '@covalent/core', `~${covalentCoreVersion}`),
+    (host: Tree) => {
+      addPackageToPackageJson(host, '@angular/material', `~${materialVersion}`);
+      addPackageToPackageJson(host, '@covalent/core', `~${covalentCoreVersion}`);
+      addPackageToPackageJson(host, '@covalent/dynamic-forms', `~${covalentCoreVersion}`);
+      addPackageToPackageJson(host, '@covalent/highlight', `~${covalentCoreVersion}`);
+      addPackageToPackageJson(host, '@covalent/http', `~${covalentCoreVersion}`);
+      addPackageToPackageJson(host, '@covalent/markdown', `~${covalentCoreVersion}`);
+      addPackageToPackageJson(host, '@covalent/flavored-markdown', `~${covalentCoreVersion}`);
+    },
     mergeWith(url('./files')),
   ]);
 }
