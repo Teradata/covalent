fd216c43acd241497e3cefe6cadb6f968a7063c1
José Toledo Navarro <jotatoledon@gmail.com>
2019-07-22 23:56:09 +0200

    *BREAKING CHANGE* refactor(media): service provided in root (#1406)
    * Since v6 angular provides a new way of registering singleton services
    by means of the provide in root module pattern.

--- src/platform/core/media/media.module.ts
+++ src/platform/core/media/media.module.ts
@@ -1,9 +1,6 @@
 import { Type } from '@angular/core';
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import { CommonModule } from '@angular/common';
-
-import { MEDIA_PROVIDER } from './services/media.service';
 import { TdMediaToggleDirective } from './directives/media-toggle.directive';
 
 const TD_MEDIA: Type<any>[] = [
@@ -11,17 +8,11 @@ const TD_MEDIA: Type<any>[] = [
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-  ],
   declarations: [
     TD_MEDIA,
   ],
   exports: [
     TD_MEDIA,
-  ],
-  providers: [
-    MEDIA_PROVIDER,
   ],
 })
 export class CovalentMediaModule {
