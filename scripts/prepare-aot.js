cca87b228f7fbd02f5230a252a8e0c4720d1bd5f
emoralesb05 <eduardo.morales@teradata.com>
2019-07-23 13:47:41 -0700

    chore(): remove not needed files from migration

--- scripts/prepare-aot.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp-help')(require('gulp'));
-var sassExtReplace = require('./sass-ext-replace.js');
-var config = require('../build.conf');
-
-gulp.task('prepare-aot', 'Steps to prepare for aot', function() {
-  sassExtReplace([config.paths.deployed]);
-});
