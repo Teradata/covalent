dbc8398164ec3832543b30f1a6eeca56b364d852
emoralesb05 <eduardo.morales@teradata.com>
2019-07-23 10:06:26 -0700

    chore(): remove not needed files from migration

--- scripts/build.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require('gulp-help')(require('gulp'));
-
-gulp.task('build', 'Compile the SASS and move required files', [
-  'compile-sass',
-  'move-required-files'
-]);
