884f5d839d21c139b1040491b94dd96ffd9c8d30
Christian Memije <christianmemije@gmail.com>
2019-08-19 10:41:38 -0700

    refactor(placeholders-script): dry up

--- scripts/version-placeholders.js
+++ scripts/version-placeholders.js
@@ -9,33 +9,28 @@ const buildConfig = require('../build.conf');
 const packageJson = require('../package.json');
 
 gulp.task('version-placeholder', function(cb) {
-  replaceVersionPlaceholders(buildConfig.deployed, packageJson.version);
+  replaceVersionPlaceholders(buildConfig.deployed);
   cb();
 });
 
-/** Variable that is set to the string for version placeholders. */
-const versionPlaceholderText = '0.0.0-COVALENT';
-const ngVersionPlaceholderText = '0.0.0-NG';
-const materialVersionPlaceholderText = '0.0.0-MATERIAL';
-const echartsVersionPlaceholderText = '0.0.0-ECHARTS';
-const showdownVersionPlaceholderText = '0.0.0-SHOWDOWN';
-const highlightVersionPlaceholderText = '0.0.0-HIGHLIGHT';
-const monacoVersionPlaceholderText = '0.0.0-MONACO';
-const simplemdeVersionPlaceholderText = '0.0.0-SIMPLEMDE';
+const placeholders = [
+  ['0.0.0-COVALENT', packageJson.version],
+  ['0.0.0-NG', buildConfig.angularVersion],
+  ['0.0.0-MATERIAL', buildConfig.materialVersion],
+  ['0.0.0-ECHARTS', buildConfig.echartsVersion],
+  ['0.0.0-SHOWDOWN', buildConfig.showdownVersion],
+  ['0.0.0-HIGHLIGHT', buildConfig.highlightVersion],
+  ['0.0.0-MONACO', buildConfig.monacoVersion],
+  ['0.0.0-SIMPLEMDE', buildConfig.simplemdeVersion],
+];
 
-/** RegExp that matches version placeholders inside of a file. */
-const versionPlaceholderRegex = new RegExp(versionPlaceholderText, 'g');
-const ngVersionPlaceholderRegex = new RegExp(ngVersionPlaceholderText, 'g');
-const materialVersionPlaceholderRegex = new RegExp(materialVersionPlaceholderText, 'g');
-const echartsVersionPlaceholderRegex = new RegExp(echartsVersionPlaceholderText, 'g');
-const showdownVersionPlaceholderRegex = new RegExp(showdownVersionPlaceholderText, 'g');
-const highlightVersionPlaceholderRegex = new RegExp(highlightVersionPlaceholderText, 'g');
-const monacoVersionPlaceholderRegex = new RegExp(monacoVersionPlaceholderText, 'g');
-const simplemdeVersionPlaceholderRegex = new RegExp(simplemdeVersionPlaceholderText, 'g');
+/** RegExps that match version placeholders inside of a file. */
+const placeholderRegexes = placeholders.map((placeholder) => new RegExp(placeholder[0], 'g'));
+
 /**
  * Walks through every file in a directory and replaces the version placeholders
  */
-function replaceVersionPlaceholders(packageDir, projectVersion) {
+function replaceVersionPlaceholders(packageDir) {
   // Resolve files that contain version placeholders using Grep or Findstr since those are
   // extremely fast and also have a very simple usage.
   const files = findFilesWithPlaceholders(packageDir);
@@ -43,16 +38,10 @@ function replaceVersionPlaceholders(packageDir, projectVersion) {
   // Walk through every file that contains version placeholders and replace those with the current
   // version of the root package.json file.
   files.forEach((filePath) => {
-    const fileContent = readFileSync(filePath, 'utf-8')
-      .replace(ngVersionPlaceholderRegex, buildConfig.angularVersion)
-      .replace(materialVersionPlaceholderRegex, buildConfig.materialVersion)
-      .replace(echartsVersionPlaceholderRegex, buildConfig.echartsVersion)
-      .replace(showdownVersionPlaceholderRegex, buildConfig.showdownVersion)
-      .replace(highlightVersionPlaceholderRegex, buildConfig.highlightVersion)
-      .replace(monacoVersionPlaceholderRegex, buildConfig.monacoVersion)
-      .replace(simplemdeVersionPlaceholderRegex, buildConfig.simplemdeVersion)
-      .replace(versionPlaceholderRegex, projectVersion);
-
+    const fileContent = placeholderRegexes.reduce(
+      (accumulator, currentValue, currentIndex) => accumulator.replace(currentValue, placeholders[currentIndex][1]),
+      readFileSync(filePath, 'utf-8'),
+    );
     writeFileSync(filePath, fileContent);
   });
 }
@@ -71,20 +60,12 @@ function buildPlaceholderFindCommand(packageDir) {
   if (platform() === 'win32') {
     return {
       binary: 'findstr',
-      args: [
-        '/msi',
-        `${materialVersionPlaceholderText} ${ngVersionPlaceholderText} ${echartsVersionPlaceholderText} ${versionPlaceholderText}`,
-        `${packageDir}\\*`,
-      ],
+      args: ['/msi', placeholders.map((item) => item[0]).join(' '), `${packageDir}\\*`],
     };
   } else {
     return {
       binary: 'grep',
-      args: [
-        '-ril',
-        `${materialVersionPlaceholderText}\\|${echartsVersionPlaceholderText}\\|${ngVersionPlaceholderText}\\|${versionPlaceholderText}`,
-        packageDir,
-      ],
+      args: ['-ril', placeholders.map((item) => item[0]).join(`\\|`), packageDir],
     };
   }
 }
