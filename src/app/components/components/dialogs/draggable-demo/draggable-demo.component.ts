9c52a4de2e1923207a5e3d1ce1a7aff4656fe3d5
Christian Memije <christianmemije@gmail.com>
2019-09-27 14:29:14 -0700

    docs(draggable-dialog): add code for draggable demo

--- src/app/components/components/dialogs/draggable-demo/draggable-demo.component.ts
+++ src/app/components/components/dialogs/draggable-demo/draggable-demo.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-draggable-demo',
-  templateUrl: './draggable-demo.component.html',
+  template: `
+    <h2>Draggable dialog</h2>
+    <p>Draggable via the drag handle</p>
+    <button mat-icon-button class="drag-handle">
+      <mat-icon>drag_handle</mat-icon>
+    </button>
+  `,
 })
 export class DraggableDemoComponent {}
