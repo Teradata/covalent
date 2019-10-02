cca414f309313259149e93fcc20c28a5b0a5496a
Priyanka <aqupriyanka@gmail.com>
2019-10-02 14:39:45 -0700

    feat(echarts): add pie chart type (#1484)
    * feat(pie_chart): add pie charts to covalent
    
    * revert package lock json
    
    * fix(indentation): fix formatting
    
    * fix(comments): update echarts documentation urls
    
    * fix(review_comments): fix echarts route and layout of pie charts
    
    * fix(documentation): remove x axis, y axis tags from pie docs
    
    * fix(icon): remove tooltip icon
    
    * fix(components): remove covalent echarts from components
    
    * fix(sample_code): add dataset end tag

--- src/platform/echarts/public-api.ts
+++ src/platform/echarts/public-api.ts
@@ -1,5 +1,6 @@
 export * from './base/index';
 export * from './bar/index';
+export * from './pie/index';
 export * from './line/index';
 export * from './scatter/index';
 export * from './tree/index';
