a7ec316d5b25503fac1db8434cb2f6f9ccd07e75
Christian Memije <christianmemije@gmail.com>
2019-10-16 15:38:27 -0700

    docs(): add components that start with n's and t's

--- src/app/content/components/components.ts
+++ src/app/content/components/components.ts
@@ -7,9 +7,10 @@ export const componentRouteCategories = [
   { name: 'Navigation', nested: false },
   { name: 'Dialogs', nested: false },
   { name: 'Forms', nested: false },
+  { name: 'Editors', nested: false },
 ];
 
-const [root, layout, buttons, nav, dialogs, forms] = componentRouteCategories;
+const [root, layout, buttons, nav, dialogs, forms, editors] = componentRouteCategories;
 
 export const componentDetails: any = [
   {
@@ -37,6 +38,55 @@ export const componentDetails: any = [
     icon: '',
     category: 'Buttons & Indicators',
     route: '/components/breadcrumbs',
+  },
+  {
+    name: 'Nav Steps',
+    id: 'nav-steps',
+    description:
+      'Navigate across a sequence of logical & numbered steps (shrink width of page to see responsive behavior)',
+    apiDocUrl: 'platform/core/steps/README.md',
+    overviewDocUrl: '',
+    showExampleTab: true,
+    showOverviewDemo: true,
+    icon: 'format_line_spacing',
+    category: nav.name,
+    route: '/components/nav-steps',
+  },
+  {
+    name: 'Notifications',
+    id: 'notifications',
+    description: 'Notification count & menu for toolbar',
+    apiDocUrl: 'platform/core/notifications/README.md',
+    overviewDocUrl: '',
+    showExampleTab: true,
+    showOverviewDemo: true,
+    icon: 'notifications',
+    category: buttons.name,
+    route: '/components/notifications',
+  },
+  {
+    name: 'Tab Select',
+    id: 'tab-select',
+    description: 'Bind values to tabs and use them as filters',
+    apiDocUrl: 'platform/core/tab-select/README.md',
+    overviewDocUrl: '',
+    showExampleTab: true,
+    showOverviewDemo: true,
+    icon: 'tab',
+    category: forms.name,
+    route: '/components/tab-select',
+  },
+  {
+    name: 'Text Editor',
+    id: 'text-editor',
+    description: 'Simple markdown text editor component (edit the markdown in the left editor for a real-time preview)',
+    apiDocUrl: 'platform/text-editor/README.md',
+    overviewDocUrl: '',
+    showExampleTab: true,
+    showOverviewDemo: true,
+    icon: 'tab',
+    category: editors.name,
+    route: '/components/text-editor',
   },
 ];
 
