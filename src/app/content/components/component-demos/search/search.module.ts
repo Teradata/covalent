668b91fbc6347e8786a661f29d6243a45835f2ca
so186021 <steven.ov@teradata.com>
2019-10-16 16:56:48 -0700

    docs(): add compoents that start with p's and s's

--- src/app/content/components/component-demos/search/search.module.ts
+++ src/app/content/components/component-demos/search/search.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes, Route } from '@angular/router';
-
-import { MatSelectModule } from '@angular/material/select';
-import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, Routes } from '@angular/router';
 
 import { ComponentDetailsModule } from 'app/components/shared/component-details/component-details.module';
-import { CovalentBreadcrumbsModule } from '@covalent/core/breadcrumbs';
+import { CovalentSearchModule } from '@covalent/core/search';
 import { setComponentRoutes } from 'app/content/components/components';
 import { SearchDemoComponent } from './search.component';
 
 const routes: Routes = setComponentRoutes({
   overviewDemoComponent: SearchDemoComponent,
-  id: 'breadcrumbs',
+  id: 'search',
 });
 
 @NgModule({
@@ -20,11 +17,9 @@ const routes: Routes = setComponentRoutes({
   imports: [
     CommonModule,
     // Material
-    MatSelectModule,
-    MatIconModule,
     ComponentDetailsModule,
     // Covalent
-    CovalentBreadcrumbsModule,
+    CovalentSearchModule,
     // Docs
     // Routes
     RouterModule.forChild(routes),
