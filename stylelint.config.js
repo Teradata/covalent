52e81eb7635367ccdfb33285c2d0964b30e94b0a
Christian Memije <christianmemije@gmail.com>
2019-10-23 13:15:11 -0700

    fix(stylelint): include stylelint config in build & convert to js

---
+++ stylelint.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['./src/platform/core/common/configs/stylelint/stylelint.config.js', 'stylelint-config-prettier'],
+  ignoreFiles: './src/platform/core/schematics/ng-add/files/**',
+};
