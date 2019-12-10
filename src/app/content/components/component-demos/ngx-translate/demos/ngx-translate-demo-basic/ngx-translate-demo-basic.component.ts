f612515c8c0a95fdb0b2346ba33d871dda7ac35c
Phillip Dominguez <felips.110@gmail.com>
2019-12-10 06:55:35 -0800

    refactor: remobing ngx translate

--- src/app/content/components/component-demos/ngx-translate/demos/ngx-translate-demo-basic/ngx-translate-demo-basic.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-translate-demo-basic',
-  styleUrls: ['./ngx-translate-demo-basic.component.scss'],
-  templateUrl: './ngx-translate-demo-basic.component.html',
-  preserveWhitespaces: true,
-})
-export class NgxTranslateDemoBasicComponent {}
--- src/app/content/components/component-demos/ngx-translate/demos/ngx-translate-demo.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-translate-demo',
-  styleUrls: ['./ngx-translate-demo.component.scss'],
-  templateUrl: './ngx-translate-demo.component.html',
-})
-export class NgxTranslateDemoComponent {}
