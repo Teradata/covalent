9b407c5170d32b63796973da6bd2551a0a0041ec
emoralesb05 <eduardo.morales@teradata.com>
2020-02-13 11:15:24 -0800

    test(): add test to verify angular issue fix

--- src/app/content/components/component-demos/loading/demos/loading-demo.module.ts
+++ src/app/content/components/component-demos/loading/demos/loading-demo.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingDemoBasicComponent } from './loading-demo-basic/loading-demo-basic.component';
 import { LoadingDemoReplaceComponent } from './loading-demo-replace/loading-demo-replace.component';
+import { LoadingDemoObservableComponent } from './loading-demo-observable/loading-demo-observable.component';
 import { CovalentLoadingModule } from '@covalent/core/loading';
 import { LoadingDemoComponent } from './loading-demo.component';
 import { LoadingDemoRoutingModule } from './loading-demo-routing.module';
@@ -14,6 +15,7 @@ import { LoadingDemoFullscreenComponent } from './loading-demo-fullscreen/loadin
     LoadingDemoComponent,
     LoadingDemoBasicComponent,
     LoadingDemoReplaceComponent,
+    LoadingDemoObservableComponent,
     LoadingDemoFullscreenComponent,
   ],
   imports: [
