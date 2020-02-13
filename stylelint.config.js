b2d0f9c9156bd0514ac4ab4f75dadf92927b0152
Christian Memije <christianmemije@users.noreply.github.com>
2020-02-13 15:07:31 -0800

    feat(stylelint): update rules (#1623)

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   extends: ['./src/platform/coding-standards/stylelint/stylelint.config.js'],
-  ignoreFiles: './src/platform/core/schematics/ng-add/files/**',
+  ignoreFiles: ['./src/platform/core/schematics/ng-add/files/**'],
 };
