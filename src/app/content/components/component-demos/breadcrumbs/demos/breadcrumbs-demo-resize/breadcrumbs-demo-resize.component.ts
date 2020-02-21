721bca81412d4fcfa377c473ca351c0af9191cf4
jk250044 <julie.knowles@teradata.com>
2020-02-20 16:43:09 -0800

    fix(breadcrumbs): add missing demos

---
+++ src/app/content/components/component-demos/breadcrumbs/demos/breadcrumbs-demo-resize/breadcrumbs-demo-resize.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'breadcrumbs-demo-resize',
+  styleUrls: ['./breadcrumbs-demo-resize.component.scss'],
+  templateUrl: './breadcrumbs-demo-resize.component.html',
+  preserveWhitespaces: true,
+})
+export class BreadcrumbsResizeBasicComponent {
+  count: number = 0;
+}
