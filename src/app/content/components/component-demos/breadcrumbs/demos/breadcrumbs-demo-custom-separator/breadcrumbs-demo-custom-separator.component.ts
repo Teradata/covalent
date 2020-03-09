940a60db9e3f017e5b8ce7eed1a808714201c129
Julie Knowles <JulieMarie@users.noreply.github.com>
2020-03-09 12:11:32 -0700

    fix(breadcrumbs): add missing demos (#1648)
    Co-authored-by: jk250044 <julie.knowles@teradata.com>

---
+++ src/app/content/components/component-demos/breadcrumbs/demos/breadcrumbs-demo-custom-separator/breadcrumbs-demo-custom-separator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'breadcrumbs-demo-custom-separator',
+  styleUrls: ['./breadcrumbs-demo-custom-separator.component.scss'],
+  templateUrl: './breadcrumbs-demo-custom-separator.component.html',
+  preserveWhitespaces: true,
+})
+export class BreadcrumbsDemoCustomSeparatorComponent {}
