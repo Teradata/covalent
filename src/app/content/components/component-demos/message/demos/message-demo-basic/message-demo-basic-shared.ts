92d70b9c84dd9954351e431ee481a6d32c3f0efc
Lennan, Thomas R <Thomas.Lennan@Teradata.com>
2020-03-15 08:02:40 -0700

    feat(message-docs): removed unnecessary import

--- src/app/content/components/component-demos/message/demos/message-demo-basic/message-demo-basic-shared.ts
+++ src/app/content/components/component-demos/message/demos/message-demo-basic/message-demo-basic-shared.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
