64f87161573e412c5854960fbd6216579053b194
Phil Dominguez <felips.110@gmail.com>
2020-03-16 13:54:02 -0700

    docs(notification): adding old demos back (#1649)
    * feat(notification-demos): adding old demos back
    
    * fix: cleanup and requested changes

---
+++ src/app/content/components/component-demos/notifications/demos/notifications-demo-toolbar/notifications-demo-toolbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'notifications-demo-toolbar',
+  styleUrls: ['./notifications-demo-toolbar.component.scss'],
+  templateUrl: './notifications-demo-toolbar.component.html',
+})
+export class NotificationsDemoToolbarComponent {}
