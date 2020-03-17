fdf0d1696a1881a3f2c23eba642c60c7e0ee3d68
Julie Knowles <JulieMarie@users.noreply.github.com>
2020-03-17 14:45:40 -0700

    docs(simple-dialogs): add demos back (#1676)
    * fix(simple-dialogs): add demos back
    
    * feat(dialog): add draggable and resizable demos
    
    * fix(dialogs): removing unused animaton
    
    * fix(simple-dialogs): cleaned up code
    
    * feat(): changed wording
    
    Co-authored-by: jk250044 <julie.knowles@teradata.com>
    Co-authored-by: Christian Memije <christianmemije@gmail.com>

--- src/app/content/components/component-demos/dialogs/demos/dialogs-demo.module.ts
+++ src/app/content/components/component-demos/dialogs/demos/dialogs-demo.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { DialogsDemoBasicComponent } from './dialogs-demo-basic/dialogs-demo-basic.component';
+import { MatIconModule } from '@angular/material/icon';
 import { CovalentDialogsModule } from '@covalent/core/dialogs';
 import { DialogsDemoComponent } from './dialogs-demo.component';
+import {
+  DialogsDemoDraggableComponent,
+  DraggableDialogComponent,
+} from './dialogs-demo-draggable/dialogs-demo-draggable.component';
+import {
+  DraggableResizableDialogComponent,
+  DialogsDemoDraggableResizableComponent,
+} from './dialogs-demo-draggable-resizable/dialogs-demo-draggable-resizable.component';
 import { DialogsDemoRoutingModule } from './dialogs-demo-routing.module';
 import { DemoModule } from '../../../../../components/shared/demo-tools/demo.module';
+import { DialogsDemoBasicSharedModule } from './dialogs-demo-basic/dialogs-demo-basic-shared';
 
 @NgModule({
-  declarations: [DialogsDemoComponent, DialogsDemoBasicComponent],
+  declarations: [
+    DialogsDemoComponent,
+    DialogsDemoDraggableComponent,
+    DraggableDialogComponent,
+    DraggableResizableDialogComponent,
+    DialogsDemoDraggableResizableComponent,
+  ],
   imports: [
+    DialogsDemoBasicSharedModule,
     DemoModule,
     DialogsDemoRoutingModule,
     /** Covalent Modules */
@@ -17,6 +33,7 @@ import { DemoModule } from '../../../../../components/shared/demo-tools/demo.mod
     /** Angular Modules */
     CommonModule,
     MatButtonModule,
+    MatIconModule,
   ],
 })
 export class DialogsDemoModule {}
