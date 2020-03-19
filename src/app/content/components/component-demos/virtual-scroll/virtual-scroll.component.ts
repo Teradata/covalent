9ebbc18ed7ec023fc992143c953deaf340579e4e
kw250042 <kris.winbush@teradata.com>
2020-03-19 15:04:16 -0700

    fix(merge): merged developed and remvoed root html, scss files

--- src/app/content/components/component-demos/virtual-scroll/virtual-scroll.component.ts
+++ src/app/content/components/component-demos/virtual-scroll/virtual-scroll.component.ts
@@ -2,7 +2,5 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'virtual-scroll',
-  templateUrl: './virtual-scroll.component.html',
-  styleUrls: ['./virtual-scroll.component.scss'],
 })
 export class VirtualScrollDemoComponent {}
--- src/app/content/components/component-demos/virtual-scroll/virtual-scroll.module.ts
+++ src/app/content/components/component-demos/virtual-scroll/virtual-scroll.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = setComponentRoutes({
 });
 
 @NgModule({
-  declarations: [VirtualScrollDemoComponent],
+  declarations: [],
   imports: [
     CommonModule,
     ComponentDetailsModule,
