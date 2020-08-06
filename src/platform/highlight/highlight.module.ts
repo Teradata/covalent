487e94955cb06cbb2d0e3408b6dd9b5690adccb2
Priyanka Arora <priyanka.arora@teradata.com>
2020-08-06 13:48:22 -0700

    fix(copy): add tooltip for copy button

--- src/platform/highlight/highlight.module.ts
+++ src/platform/highlight/highlight.module.ts
@@ -4,9 +4,10 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
 import { TdHighlightComponent } from './highlight.component';
 import { HighlightClipboardComponent } from './highlight-clipboard/highlight-clipboard.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  imports: [CommonModule, ClipboardModule, MatIconModule],
+  imports: [CommonModule, ClipboardModule, MatIconModule, MatTooltipModule],
   declarations: [TdHighlightComponent, HighlightClipboardComponent],
   exports: [TdHighlightComponent, HighlightClipboardComponent],
 })
