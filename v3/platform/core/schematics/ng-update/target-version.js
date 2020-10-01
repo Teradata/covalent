77cd39651431f1f429a22bee9e74af8a2acd8ae8
emoralesb05 <eduardo.morales@teradata.com>
2020-10-01 14:13:40 -0700

    fix(build): remove files pushed by mistake

--- v3/platform/core/schematics/ng-update/target-version.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var TargetVersion;
-(function (TargetVersion) {
-  TargetVersion['V3'] = 'version 3.0.0';
-})((TargetVersion = exports.TargetVersion || (exports.TargetVersion = {})));
-//# sourceMappingURL=target-version.js.map
--- v3/platform/core/schematics/version-names.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.covalentCoreVersion = '3.0.0';
-exports.materialVersion = '9.0.0';
-//# sourceMappingURL=version-names.js.map
