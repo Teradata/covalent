daef5f7a5a6670637641f76030afbc9d0042741c
oseer <oseer.williams@teradata.com>
2022-01-27 09:26:47 -0500

    refactor(expirements): removing old expirements, unused test bed

--- src/test-bed/test-bed.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
-export const appRoutingProviders: any[] = [];
-
-export const appRoutes: any = RouterModule.forRoot(routes, {
-  useHash: true,
-});
