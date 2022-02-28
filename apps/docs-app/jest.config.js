a19767fc16e4dc82204ad89927747d28d59e8390
oseer <oseer.williams@teradata.com>
2022-02-28 12:40:37 -0500

    feat(nx): renaming docs app

--- apps/docs-app/jest.config.js
+++ apps/docs-app/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'docs',
+  displayName: 'docs-app',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/docs',
+  coverageDirectory: '../../coverage/apps/docs-app',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
