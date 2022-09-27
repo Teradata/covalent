a15e02239b94b9ac0141d001590c8a23a8877e75
Nunna, Sailaja <sailaja.nunna@teradata.com>
2022-09-27 15:34:11 -0400

    fix(nx): side sheet behavior change

--- apps/docs-app/src/app/content/components/component-demos/side-sheet/demos/side-sheet-demo-multi/side-sheet-demo-multi.component.ts
+++ apps/docs-app/src/app/content/components/component-demos/side-sheet/demos/side-sheet-demo-multi/side-sheet-demo-multi.component.ts
@@ -1,31 +1,62 @@
 import { Component } from '@angular/core';
-import { CovalentSideSheet } from '@covalent/core/side-sheet';
+import { SubPageMode, CovalentSideSheet } from '@covalent/core/side-sheet';
 
 @Component({
   selector: 'example',
   template: `
     it works from a component
-    <div style="display:flex; justify-content:space-between;">
-      <button mat-raised-button color="primary" (click)="openSideSheet()">
-        Open
-      </button>
+
+    <div style="display:flex; justify-content:right; padding-top:50px">
       <button mat-raised-button td-side-sheet-close>Close</button>
+    </div>
+
+    <div style="display:flex; justify-content:space-between; padding-top:50px">
+      <button
+        mat-raised-button
+        color="primary"
+        (click)="openSideSheet('pushed')"
+      >
+        Open pushed
+      </button>
+    </div>
+
+    <div style="display:flex; justify-content:space-between; padding-top:50px">
+      <button
+        mat-raised-button
+        color="primary"
+        (click)="openSideSheet('shifted')"
+      >
+        Open shifted
+      </button>
+    </div>
+
+    <div style="display:flex; justify-content:space-between; padding-top:50px">
+      <button mat-raised-button color="primary" (click)="openSideSheet('none')">
+        Open none
+      </button>
     </div>
   `,
 })
 export class SideSheetDemoExampleComponent {
   constructor(private sideSheet: CovalentSideSheet) {}
 
-  openSideSheet(): void {
+  openSideSheet(mode?: string): void {
+    let subPageMode: SubPageMode = SubPageMode.pushed;
+    if (mode === 'shifted') {
+      subPageMode = SubPageMode.shifted;
+    } else if (mode === 'none') {
+      subPageMode = SubPageMode.none;
+    }
     this.sideSheet.open(SideSheetDemoExampleLayeredComponent, {
-      minWidth: '800px',
+      minWidth: '400px',
+      subPageMode: subPageMode,
     });
   }
 }
 
 @Component({
   selector: 'example-layered',
-  template: 'Im Mulit layered!',
+  template: 'Im Multi layered!',
 })
 class SideSheetDemoExampleLayeredComponent {}
 
