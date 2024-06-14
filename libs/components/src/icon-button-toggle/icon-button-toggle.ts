ceab11d9c4daa114c279de9c85e65f3d793bfa15
bsahitya <sahitya.buddharaju@gmail.com>
2024-06-14 09:58:58 -0500

    fix(icon-button-toggle): use update method to sync on and toggledOn

--- libs/components/src/icon-button-toggle/icon-button-toggle.ts
+++ libs/components/src/icon-button-toggle/icon-button-toggle.ts
@@ -1,8 +1,6 @@
-import { css, html, unsafeCSS } from 'lit';
+import { css, html, PropertyValues, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { IconButtonToggle } from '@material/mwc-icon-button-toggle/mwc-icon-button-toggle';
-import { classMap } from 'lit/directives/class-map.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
 import styles from './icon-button-toggle.scss?inline';
 
 declare global {
@@ -27,17 +25,17 @@ export class CovalentIconButtonToggle extends IconButtonToggle {
     `,
   ];
 
-  protected override handleClick(): void {
-    this.toggledOn = !this.toggledOn;
-    this.dispatchEvent(
-      new CustomEvent('icon-button-toggle-change', {
-        detail: { isOn: this.toggledOn },
-        bubbles: true,
-      })
-    );
+  protected update(changedProperties: PropertyValues) {
+    if (changedProperties.has('toggledOn') && this.toggledOn !== this.on) {
+      this.on = this.toggledOn;
+    }
+    if (changedProperties.has('on') && this.toggledOn !== this.on) {
+      this.toggledOn = this.on;
+    }
+    super.update(changedProperties);
   }
 
-  protected override renderRipple() {
+  protected renderRipple() {
     return this.shouldRenderRipple
       ? html` <mwc-ripple
           .disabled="${this.disabled}"
@@ -46,48 +44,6 @@ export class CovalentIconButtonToggle extends IconButtonToggle {
         >
         </mwc-ripple>`
       : '';
-  }
-
-  protected override render() {
-    /** @classMap */
-    const classes = {
-      'mdc-icon-button--on': this.toggledOn,
-    };
-    const hasToggledAriaLabel =
-      this.ariaLabelOn !== undefined && this.ariaLabelOff !== undefined;
-    const ariaPressedValue = hasToggledAriaLabel ? undefined : this.toggledOn;
-    const ariaLabelValue = hasToggledAriaLabel
-      ? this.toggledOn
-        ? this.ariaLabelOn
-        : this.ariaLabelOff
-      : this.ariaLabel;
-    return html`<button
-      class="mdc-icon-button mdc-icon-button--display-flex ${classMap(classes)}"
-      aria-pressed="${ifDefined(ariaPressedValue)}"
-      aria-label="${ifDefined(ariaLabelValue)}"
-      @click="${this.handleClick}"
-      ?disabled="${this.disabled}"
-      @focus="${this.handleRippleFocus}"
-      @blur="${this.handleRippleBlur}"
-      @mousedown="${this.handleRippleMouseDown}"
-      @mouseenter="${this.handleRippleMouseEnter}"
-      @mouseleave="${this.handleRippleMouseLeave}"
-      @touchstart="${this.handleRippleTouchStart}"
-      @touchend="${this.handleRippleDeactivate}"
-      @touchcancel="${this.handleRippleDeactivate}"
-    >
-      ${this.renderRipple()}
-      <span class="mdc-icon-button__icon"
-        ><slot name="offIcon"
-          ><i class="material-icons">${this.offIcon}</i></slot
-        ></span
-      >
-      <span class="mdc-icon-button__icon mdc-icon-button__icon--on"
-        ><slot name="onIcon"
-          ><i class="material-icons">${this.onIcon}</i></slot
-        ></span
-      >
-    </button>`;
   }
 }
 
