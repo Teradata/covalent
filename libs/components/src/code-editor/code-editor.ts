99f0cd1990afaff65faab6bf2b585bf3a71abf91
Sahitya Buddharaju <sahitya.buddharaju@gmail.com>
2024-07-08 20:30:48 -0500

    fix(code-editor): register supported programming languages

--- libs/components/src/code-editor/code-editor.ts
+++ libs/components/src/code-editor/code-editor.ts
@@ -5,8 +5,32 @@ import { createRef, Ref, ref } from 'lit/directives/ref.js';
 import styles from './code-editor.scss?inline';
 
 // -- Monaco Editor Imports --
-import * as monaco from 'monaco-editor';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
 import baseStyles from 'monaco-editor/min/vs/editor/editor.main.css?inline';
+
+// Register all language contributions
+import 'monaco-editor/esm/vs/basic-languages/html/html.contribution';
+import 'monaco-editor/esm/vs/basic-languages/css/css.contribution';
+import 'monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution';
+import 'monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution';
+import 'monaco-editor/esm/vs/basic-languages/python/python.contribution';
+import 'monaco-editor/esm/vs/basic-languages/java/java.contribution';
+import 'monaco-editor/esm/vs/basic-languages/csharp/csharp.contribution';
+import 'monaco-editor/esm/vs/basic-languages/sql/sql.contribution';
+import 'monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution';
+import 'monaco-editor/esm/vs/basic-languages/xml/xml.contribution';
+import 'monaco-editor/esm/vs/basic-languages/markdown/markdown.contribution';
+import 'monaco-editor/esm/vs/basic-languages/shell/shell.contribution';
+import 'monaco-editor/esm/vs/basic-languages/go/go.contribution';
+import 'monaco-editor/esm/vs/basic-languages/r/r.contribution';
+import 'monaco-editor/esm/vs/basic-languages/cpp/cpp.contribution';
+import 'monaco-editor/esm/vs/basic-languages/rust/rust.contribution';
+import 'monaco-editor/esm/vs/basic-languages/kotlin/kotlin.contribution';
+import 'monaco-editor/esm/vs/basic-languages/swift/swift.contribution';
+import 'monaco-editor/esm/vs/basic-languages/lua/lua.contribution';
+import 'monaco-editor/esm/vs/basic-languages/scala/scala.contribution';
+import 'monaco-editor/esm/vs/basic-languages/dart/dart.contribution';
+import 'monaco-editor/esm/vs/basic-languages/php/php.contribution';
 
 @customElement('cv-code-editor')
 export class CovalentCodeEditor extends LitElement {
@@ -96,6 +120,7 @@ export class CovalentCodeEditor extends LitElement {
         'editor.background': '#f6f3f6ff',
       },
     });
+
     this.editor = monaco.editor.create(container, {
       ...this.options,
       fontLigatures: '',
