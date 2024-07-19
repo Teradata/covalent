44aea1ed3d2737c6d777c713993f7c0b217a7d10
Julie Knowles <JulieMarie@users.noreply.github.com>
2024-07-19 09:42:53 -0700

    fix(markdown): relative href links in md and minor styling (#2194)

--- libs/angular/dialogs/src/resizable-draggable-dialog/resizable-draggable-dialog.ts
+++ libs/angular/dialogs/src/resizable-draggable-dialog/resizable-draggable-dialog.ts
@@ -144,7 +144,7 @@ export class ResizableDraggableDialog {
         rightLeft = horizontalAlignment.right;
 
         const icon: HTMLElement = this._renderer2.createElement('i');
-        this._renderer2.addClass(icon, 'material-icons');
+        this._renderer2.addClass(icon, 'material-icons-outlined');
         this._renderer2.appendChild(
           icon,
           this._renderer2.createText('filter_list')
