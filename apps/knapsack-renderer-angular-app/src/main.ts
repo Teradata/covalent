810909f01454e76cd3fb285a115f424230acb3fd
Josh Mabry <mabry1985@gmail.com>
2024-09-04 16:52:20 -0700

    docs: removes ks angular app

--- apps/knapsack-renderer-angular-app/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { KsAppModule } from './dist/ks-ng-app.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(KsAppModule)
-  .catch((err) => console.error(err));
