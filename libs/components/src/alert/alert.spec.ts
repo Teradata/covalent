2cf6f999347a2e2858e91123b50c8d6470b87544
Oseer Williams <oseer.williams@gmail.com>
2025-02-13 10:41:47 -0500

    build(jest): mock resize observer

--- libs/components/src/alert/alert.spec.ts
+++ libs/components/src/alert/alert.spec.ts
@@ -4,6 +4,21 @@
 import { it, describe, expect } from 'vitest';
 import { CovalentAlert } from './alert';
 
+global.ResizeObserver = class MockResizeObserver {
+  constructor() {
+    /**/
+  }
+  observe() {
+    /**/
+  }
+  unobserve() {
+    /**/
+  }
+  disconnect() {
+    /**/
+  }
+};
+
 describe('Alert', () => {
   it('should work', () => {
     expect(new CovalentAlert()).toBeDefined();
