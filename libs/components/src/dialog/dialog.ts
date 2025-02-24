5f29a8f7a26bef819179c599ba138a4b4f538451
Oseer Williams <oseer.williams@gmail.com>
2025-02-24 17:17:52 -0500

    fix(dialog): remove dialog from blocking elements

--- libs/components/src/dialog/dialog.ts
+++ libs/components/src/dialog/dialog.ts
@@ -1,4 +1,4 @@
-import { css, PropertyValues, unsafeCSS } from 'lit';
+import { css, unsafeCSS } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { DialogBase } from '@material/mwc-dialog/mwc-dialog-base';
 import { styles as baseStyles } from '@material/mwc-dialog/mwc-dialog.css';
