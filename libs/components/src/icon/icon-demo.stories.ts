9a7107f958c06ab70ce76ef1fb809bb5e42075fc
Oseer Williams <oseer.williams@gmail.com>
2025-06-23 12:35:14 -0400

    feat(components): add icon demo component and update icon lists

---
+++ libs/components/src/icon/icon-demo.stories.ts
@@ -0,0 +1,37 @@
+import { LitElement, css, html, unsafeCSS } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import COV_ICON_LIST from './_icon-list.stories';
+import styles from './icon-demo.scss?inline';
+import './icon';
+import '../typography/typography';
+
+@customElement('cv-icon-demo')
+export class CovalentIconDemo extends LitElement {
+  static override styles = [
+    css`
+      ${unsafeCSS(styles)}
+    `,
+  ];
+
+  render() {
+    return html`<div class="icon-grid">
+      ${COV_ICON_LIST.map(
+        (icon) =>
+          html`<div class="icon-container">
+            <cv-icon>${icon}</cv-icon>
+            <cv-typography scale="caption">
+              <div class="wrap">${icon}</div>
+            </cv-typography>
+          </div>`,
+      )}
+    </div>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'cv-icon-demo': CovalentIconDemo;
+  }
+}
+
+export default CovalentIconDemo;
--- libs/components/vite.config.js
+++ libs/components/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import fs from 'fs';
 import path from 'path';
 
@@ -26,6 +27,12 @@ module.exports = defineConfig(({ mode }) => {
     optimizeDeps: {
       include: ['monaco-editor'], // Ensure Monaco Editor is pre-bundled
     },
+    plugins: [
+      dts({
+        tsconfigPath: 'libs/components/tsconfig.lib.json',
+        // Other options like include, exclude, rollupTypes, etc.
+      }),
+    ],
     build: {
       lib: {
         entry: ['libs/components/src/index.ts', ...getComponentEntries()],
