8c61b14dba3261b5ddb6c7bc64e82c69f2784c63
Manuel Salazar <jesus.salazar@bairesde.com>
2025-07-02 09:34:32 -0700

    feat: implement CovalentDivider component with SCSS styles

--- libs/components/src/divider/divider.ts
+++ libs/components/src/divider/divider.ts
@@ -1,4 +1,6 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css, unsafeCSS } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import styles from './divider.component.scss?inline';
 
 /**
  * CovalentDivider
@@ -6,12 +8,23 @@ import { LitElement, html } from 'lit';
  * A divider component that creates a visual separation between content.
  * Can be used horizontally or vertically with different variants.
  */
-class CovalentDivider extends LitElement {
+@customElement('cv-divider')
+export class CovalentDivider extends LitElement {
   static properties = {
     direction: { type: String },
     size: { type: String },
     flush: { type: Boolean },
   };
+
+  static override styles = [
+    css`
+      ${unsafeCSS(styles)}
+    `,
+  ];
+
+  direction!: string;
+  size!: string;
+  flush!: boolean;
 
   constructor() {
     super();
@@ -20,126 +33,10 @@ class CovalentDivider extends LitElement {
     this.flush = false;
   }
 
-  get isVertical() {
-    return this.direction === 'vertical';
-  }
-
-  getSpacing() {
-    const { isVertical, size, flush } = this;
-
-    let spacingX = isVertical ? '8px' : '0';
-    let spacingY = isVertical ? '0' : '8px';
-
-    if (flush) {
-      if (isVertical) {
-        spacingX = '0';
-      } else {
-        spacingY = '0';
-      }
-    }
-
-    let spacingLeft = spacingX;
-    let spacingRight = spacingX;
-    let spacingTop = spacingY;
-    let spacingBottom = spacingY;
-
-    if (size === 'inset') {
-      if (isVertical) {
-        spacingLeft = flush ? '0' : '8px';
-        spacingRight = flush ? '0' : '8px';
-        spacingTop = '12px';
-        spacingBottom = '12px';
-      } else {
-        spacingLeft = '16px';
-        spacingRight = '16px';
-      }
-    }
-
-    return { spacingLeft, spacingRight, spacingTop, spacingBottom };
-  }
-
-  getLineStyles() {
-    const { isVertical, size } = this;
-
-    if (isVertical) {
-      if (size === 'icon') {
-        return `
-          top: 50%;
-          left: 50%;
-          width: 1px;
-          height: 24px;
-          transform: translateX(-50%) translateY(-50%);
-        `;
-      } else {
-        return `
-          top: var(--spacing-top);
-          bottom: var(--spacing-bottom);
-          left: 50%;
-          width: 1px;
-          transform: translateX(-50%);
-        `;
-      }
-    } else {
-      if (size === 'icon') {
-        return `
-          top: 50%;
-          left: 50%;
-          width: 24px;
-          height: 1px;
-          transform: translateX(-50%) translateY(-50%);
-        `;
-      } else {
-        return `
-          top: 50%;
-          left: var(--spacing-left);
-          right: var(--spacing-right);
-          height: 1px;
-          transform: translateY(-50%);
-        `;
-      }
-    }
-  }
-
   render() {
-    const { spacingLeft, spacingRight, spacingTop, spacingBottom } =
-      this.getSpacing();
-    const lineStyles = this.getLineStyles();
-    const isVertical = this.isVertical;
-
-    return html`
-      <style>
-        :host {
-          --spacing-left: ${spacingLeft};
-          --spacing-right: ${spacingRight};
-          --spacing-top: ${spacingTop};
-          --spacing-bottom: ${spacingBottom};
-
-          display: ${isVertical ? 'inline-block' : 'block'};
-          ${isVertical
-          ? 'min-height: 100%; max-height: 100vh; width: auto;'
-          : 'width: 100%;'}
-          padding:
-            var(--spacing-top)
-            var(--spacing-right)
-            var(--spacing-bottom)
-            var(--spacing-left);
-          margin: 0;
-          position: relative;
-          box-sizing: border-box;
-        }
-
-        :host::before {
-          content: '';
-          position: absolute;
-          background-color: var(--cv-theme-outline-variant, #D1D5E7);
-          ${lineStyles}
-        }
-      </style>
-    `;
+    return html``;
   }
 }
-
-customElements.define('cv-divider', CovalentDivider);
 
 declare global {
   interface HTMLElementTagNameMap {
