814fb1c2baee73284decbc5a68ecd23599a1efae
Sahitya Buddharaju <sahitya.buddharaju@gmail.com>
2025-09-17 18:51:14 -0500

    fix: support loading state i cv-textfiled

--- libs/components/src/textfield/textfield.ts
+++ libs/components/src/textfield/textfield.ts
@@ -1,7 +1,8 @@
-import { css, unsafeCSS } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { css, html, TemplateResult, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 import { TextFieldBase } from '@material/mwc-textfield/mwc-textfield-base';
 import { styles as baseTextFieldStyles } from '@material/mwc-textfield/mwc-textfield.css';
+import '../circular-progress/circular-progress';
 import styles from './textfield.scss?inline';
 
 declare global {
@@ -19,7 +20,32 @@ export class CovalentTextField extends TextFieldBase {
     `,
   ];
 
+  /**
+   * Displays a loading spinner within the text input field.
+   */
+  @property({ type: Boolean, reflect: true })
+  loading = false;
+
+  /**
+   * Displays a loading spinner within the text input field.
+   */
+  @property({ type: Number })
+  loaderDensity = -6;
+
   override outlined = true;
+
+  protected override renderTrailingIcon(): TemplateResult | string {
+    if (this.loading) {
+      return html`<cv-circular-progress
+        class="text-field-loading"
+        indeterminate
+        progress="0"
+        density="${this.loaderDensity}"
+      >
+      </cv-circular-progress>`;
+    }
+    return this.iconTrailing ? this.renderIcon(this.iconTrailing, true) : '';
+  }
 }
 
 export default CovalentTextField;
