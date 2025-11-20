93e5461319a1dc8f9b9d83f763dc080208fdc199
oseer williams <oseer.williams@gmail.com>
2025-11-20 14:02:38 -0500

    fix(markdown): update sanitizer context and enhance showdown options (#2592)
    * fix(markdown): update sanitizer context and enhance showdown options
    
    * build(markdown): add tests for preserving inline styles and classes
    
    * fix(markdown): add support for inline HTML/CSS in markdown demo

--- libs/markdown/src/lib/markdown.component.ts
+++ libs/markdown/src/lib/markdown.component.ts
@@ -414,9 +414,10 @@ export class TdMarkdownComponent
 
     const html: string =
       this._domSanitizer.sanitize(
-        SecurityContext.HTML,
+        SecurityContext.STYLE,
         changeStyleAlignmentToClass(markupStr),
       ) ?? '';
+
     const htmlWithAbsoluteHrefs: string = normalizeHtmlHrefs(
       html,
       this._hostedUrl,
@@ -460,6 +461,7 @@ export class TdMarkdownComponent
     // Convert markdown into html
     const converter: showdown.Converter = new showdown.Converter();
     converter.setOption('ghCodeBlocks', true);
+    converter.setOption('ghCompatibleHeaderId', true);
     converter.setOption('tasklists', true);
     converter.setOption('tables', true);
     converter.setOption('literalMidWordUnderscores', true);
