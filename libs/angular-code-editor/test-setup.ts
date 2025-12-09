0dd22c043256321ae59e78b9743a6903c7900f04
Omar Martinez <hello@omaar.dev>
2025-12-09 14:29:11 -0600

    fix(monaco-editor): simplify testing file

--- libs/angular-code-editor/test-setup.ts
+++ libs/angular-code-editor/test-setup.ts
@@ -1,37 +1,4 @@
 import 'jest-preset-angular/setup-jest';
 import 'jest-canvas-mock';
-
-// Mock CSS class for Monaco Editor 0.55.1+
-// Monaco usa CSS.supports() y otras APIs CSS que no existen en jsdom
-(globalThis as any).CSS = {
-  supports: (rule: string) => true,
-  escape: (value: string) => value,
-};
-
-// Mock ResizeObserver requerido por Monaco Editor
-(globalThis as any).ResizeObserver = class ResizeObserver {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  observe() {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  unobserve() {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  disconnect() {}
-};
-
-// Mock IntersectionObserver si es necesario
-(globalThis as any).IntersectionObserver = class IntersectionObserver {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  observe() {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  unobserve() {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  disconnect() {}
-  takeRecords() {
-    return [];
-  }
-  root = null;
-  rootMargin = '';
-  thresholds = [];
-};
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import '../testing-utils/monaco-test-setup';
