bfb9e1e9eeaf80662dec0b454ecf63bfe92b20bf
Omar Martinez <hello@omaar.dev>
2025-12-09 13:31:02 -0600

    fix(monaco-editor): fix testing suite

--- libs/angular/test-setup.ts
+++ libs/angular/test-setup.ts
@@ -1,1 +1,38 @@
 import 'jest-preset-angular/setup-jest';
+
+// Mock CSS API required by Monaco Editor 0.55.1+
+(globalThis as any).CSS = {
+  supports: (rule: string) => true,
+  escape: (value: string) => value,
+};
+
+// Mock deprecated document.queryCommandSupported used by Monaco clipboard
+document.queryCommandSupported = () => false;
+
+// Mock ResizeObserver
+(globalThis as any).ResizeObserver = class ResizeObserver {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  observe() {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  unobserve() {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  disconnect() {}
+};
+
+// Mock IntersectionObserver
+(globalThis as any).IntersectionObserver = class IntersectionObserver {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  observe() {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  unobserve() {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  disconnect() {}
+  takeRecords() {
+    return [];
+  }
+  root = null;
+  rootMargin = '';
+  thresholds = [];
+};
