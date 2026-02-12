a152ac9bf21404cfede98994106b936a468a4ff5
Diego Aguilar Torres <Diego.AguilarTorres@Teradata.com>
2026-02-12 14:18:01 -0600

    feat(storybook): update to csf3 and match props from knapsack

--- libs/components/src/icon-button-toggle/icon-button-toggle.stories.js
+++ libs/components/src/icon-button-toggle/icon-button-toggle.stories.js
@@ -12,22 +12,39 @@ export default {
       control: 'select',
       options: Object.keys(iconList),
     },
+    'aria-label': {
+      control: 'text',
+    },
+    ariaLabelOn: {
+      control: 'text',
+    },
+    ariaLabelOff: {
+      control: 'text',
+    },
   },
   args: {
     onIcon: 'flashlight_on',
     offIcon: 'flashlight_off',
     disabled: false,
     toggledOn: false,
+    'aria-label': '',
+    ariaLabelOn: '',
+    ariaLabelOff: '',
   },
   tags: ['autodocs'],
 };
 
-const Template = ({ disabled, onIcon, offIcon, toggledOn }) => {
-  return `
-    <cv-icon-button-toggle onIcon="${onIcon}" offIcon="${offIcon}"${
-      disabled ? ` disabled` : ``
-    }${toggledOn ? ' toggledOn' : ''}>
-    </cv-icon-button-toggle>`;
+const renderTemplate = (args) => {
+  const { disabled, onIcon, offIcon, toggledOn, ariaLabelOn, ariaLabelOff } =
+    args;
+  const ariaLabel = args['aria-label'];
+  return `<cv-icon-button-toggle${disabled ? ' disabled' : ''} onIcon="${onIcon}" offIcon="${offIcon}"${toggledOn ? ' toggledOn' : ''}${ariaLabel ? ` aria-label="${ariaLabel}"` : ''}${ariaLabelOn ? ` ariaLabelOn="${ariaLabelOn}"` : ''}${ariaLabelOff ? ` ariaLabelOff="${ariaLabelOff}"` : ''}></cv-icon-button-toggle>`;
 };
 
-export const Basic = Template.bind({});
+export const Main = {
+  render: renderTemplate,
+  args: {
+    onIcon: 'flashlight_on',
+    offIcon: 'flashlight_off',
+  },
+};
