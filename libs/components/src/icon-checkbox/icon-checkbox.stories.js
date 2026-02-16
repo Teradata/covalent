7a80f33bebeb3bb22d1cffea283f1576b22c08b9
Diego Aguilar Torres <Diego.AguilarTorres@Teradata.com>
2026-02-16 13:57:15 -0600

    feat(storybook): update to csf3 and match props from knapsack

--- libs/components/src/icon-checkbox/icon-checkbox.stories.js
+++ libs/components/src/icon-checkbox/icon-checkbox.stories.js
@@ -3,22 +3,65 @@ import '../icon/icon';
 
 export default {
   title: 'Components/Icon Checkbox',
+  argTypes: {
+    checked: {
+      control: 'boolean',
+      description: 'True when the icon checkbox is checked',
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Disables the icon checkbox and renders in muted colors.',
+    },
+    iconOnly: {
+      control: 'boolean',
+      description: 'Displays the icon without any text in the icon checkbox',
+    },
+    value: {
+      control: 'text',
+      description:
+        'The value that will be included if the checkbox is submitted in a form.',
+    },
+    width: {
+      control: 'number',
+      description: 'Sets the width of the icon checkbox',
+    },
+    height: {
+      control: 'number',
+      description: 'Sets the height of the icon checkbox',
+    },
+  },
   args: {
     width: 200,
     height: 160,
     iconOnly: false,
+    checked: false,
+    disabled: false,
+    value: '',
   },
   tags: ['autodocs'],
 };
 
-export const Template = ({ width, height, iconOnly }) => {
+const TemplateRender = (args) => {
+  const { width, height, iconOnly, checked, disabled, value } = args;
   return `
-  <cv-checkbox-icon  ${iconOnly ? ' iconOnly' : ''}>
+      <cv-checkbox-icon
+        ${iconOnly ? 'iconOnly' : ''}
+        ${checked ? 'checked' : ''}
+        ${disabled ? 'disabled' : ''}
+        ${value ? `value="${value}"` : ''}
+        style="width:${width}px; height:${height}px"
+      >
         <cv-icon slot="icon">work</cv-icon>
         <div slot="text">Balanced</div>
         <div slot="text">Every week</div>
       </cv-checkbox-icon>
-      <cv-checkbox-icon  ${iconOnly ? 'iconOnly' : ''}>
+      <cv-checkbox-icon  
+        ${iconOnly ? 'iconOnly' : ''} 
+        ${checked ? 'checked' : ''} 
+        ${disabled ? 'disabled' : ''} 
+        ${value ? `value="${value}"` : ''}
+        style="width:${width}px; height:${height}px"
+      >
         <cv-icon slot="icon">work</cv-icon>
         <div slot="text">Balanced</div>
         <div slot="text">Every week</div>
@@ -26,53 +69,96 @@ export const Template = ({ width, height, iconOnly }) => {
   `;
 };
 
-const GridExample = ({ iconOnly }) => {
+export const Template = {
+  render: TemplateRender,
+};
+
+export const Unchecked = {
+  render: TemplateRender,
+  args: {
+    width: 200,
+    height: 160,
+    checked: false,
+    disabled: false,
+    iconOnly: false,
+    indeterminate: false,
+    reducedTouchTarget: false,
+  },
+};
+
+export const Checked = {
+  render: TemplateRender,
+  args: {
+    width: 200,
+    height: 160,
+    checked: true,
+    disabled: false,
+    iconOnly: false,
+    indeterminate: false,
+    reducedTouchTarget: false,
+  },
+};
+
+export const Disabled = {
+  render: TemplateRender,
+  args: {
+    width: 200,
+    height: 160,
+    checked: false,
+    disabled: true,
+    iconOnly: false,
+    indeterminate: false,
+    reducedTouchTarget: false,
+  },
+};
+
+const GridExample = (args) => {
+  const { width, height, iconOnly, checked, disabled, value } = args;
+
+  const commonProps = `
+  ${iconOnly ? 'iconOnly' : ''}
+  ${checked ? 'checked' : ''}
+  ${disabled ? 'disabled' : ''}
+  ${value ? `value="${value}"` : ''}
+  style="width:${width}px; height:${height}px"
+  `;
+
   return `
-  <div class="mdc-layout-grid">
-  <div class="mdc-layout-grid__inner">
-    <cv-checkbox-icon ${
-      iconOnly ? 'iconOnly' : ''
-    } class="mdc-layout-grid__cell">
+    <div class="mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <cv-checkbox-icon ${commonProps} class="mdc-layout-grid__cell">
           <cv-icon slot="icon">storage</cv-icon>
           <div slot="text">Data</div>
         </cv-checkbox-icon>
-        <cv-checkbox-icon ${
-          iconOnly ? 'iconOnly' : ''
-        } class="mdc-layout-grid__cell">
+        <cv-checkbox-icon ${commonProps} class="mdc-layout-grid__cell">
           <cv-icon slot="icon">description</cv-icon>
           <div slot="text">Files</div>
         </cv-checkbox-icon>
-        <cv-checkbox-icon ${
-          iconOnly ? 'iconOnly' : ''
-        } class="mdc-layout-grid__cell">
+        <cv-checkbox-icon ${commonProps} class="mdc-layout-grid__cell">
           <cv-icon slot="icon">bookmark</cv-icon>
           <div slot="text">Articles</div>
         </cv-checkbox-icon>
-        <cv-checkbox-icon ${
-          iconOnly ? 'iconOnly' : ''
-        } class="mdc-layout-grid__cell">
+        <cv-checkbox-icon ${commonProps} class="mdc-layout-grid__cell">
           <cv-icon slot="icon">folder</cv-icon>
           <div slot="text">Projects</div>
         </cv-checkbox-icon>
-        <cv-checkbox-icon ${
-          iconOnly ? 'iconOnly' : ''
-        } class="mdc-layout-grid__cell">
+        <cv-checkbox-icon ${commonProps} class="mdc-layout-grid__cell">
           <cv-icon slot="icon">assignment</cv-icon>
           <div slot="text">Notebooks</div>
         </cv-checkbox-icon>
-        <cv-checkbox-icon ${
-          iconOnly ? 'iconOnly' : ''
-        } class="mdc-layout-grid__cell">
+        <cv-checkbox-icon ${commonProps} class="mdc-layout-grid__cell">
           <cv-icon slot="icon" >receipt_long</cv-icon>
           <div slot="text">Scripts</div>
         </cv-checkbox-icon>
-        </div>
-        </div>
+      </div>
+    </div>
   `;
 };
 
-export const WithGrid = GridExample.bind({});
-WithGrid.args = {
-  width: '105',
-  height: '100',
+export const WithGrid = {
+  render: GridExample,
+  args: {
+    width: 105,
+    height: 100,
+  },
 };
--- libs/components/src/icon-lockup/icon-lockup.stories.js
+++ libs/components/src/icon-lockup/icon-lockup.stories.js
@@ -4,6 +4,10 @@ import '../circular-progress/circular-progress';
 export default {
   title: 'Components/Icon lockup',
   argTypes: {
+    covalentIcon: { control: 'boolean' },
+    filledIcon: { control: 'boolean' },
+    icon: { control: 'text' },
+    trailingIcon: { control: 'boolean' },
     scale: {
       options: [
         'headline1',
@@ -32,20 +36,14 @@ export default {
     filledIcon: false,
     icon: 'houseboat',
     scale: 'body1',
-    state: 'null',
+    state: null,
     trailingIcon: false,
   },
   tags: ['autodocs'],
 };
 
-const Template = ({
-  icon,
-  scale,
-  trailingIcon,
-  state,
-  covalentIcon,
-  filledIcon,
-}) => {
+const TemplateRender = (args) => {
+  const { icon, scale, trailingIcon, state, covalentIcon, filledIcon } = args;
   return `<cv-icon-lockup icon="${icon}" scale="${scale}"${
     trailingIcon ? ' trailingIcon' : ''
   }${covalentIcon ? ' covalentIcon' : ''}${filledIcon ? ' filledIcon' : ''}${
@@ -53,7 +51,8 @@ const Template = ({
   }>Lorem ipsum dolor sit amet</cv-icon-lockup>`;
 };
 
-const LoadingTemplate = ({ scale, trailingIcon, state }) => {
+const LoadingRender = (args) => {
+  const { scale, trailingIcon, state } = args;
   return `<cv-icon-lockup scale="${scale}"${
     trailingIcon ? ' trailingIcon' : ''
   }${state ? ` state=${state}` : ''}>Lorem ipsum dolor sit amet
@@ -61,34 +60,31 @@ const LoadingTemplate = ({ scale, trailingIcon, state }) => {
   </cv-icon-lockup>`;
 };
 
-export const Basic = Template.bind({});
-
-export const Primary = Template.bind({});
-Primary.args = {
-  state: 'primary',
+export const Main = {
+  render: TemplateRender,
 };
 
-export const Positive = Template.bind({});
-Positive.args = {
-  state: 'positive',
-  icon: 'check',
+export const Primary = {
+  render: TemplateRender,
+  args: { state: 'primary' },
 };
 
-export const Negative = Template.bind({});
-Negative.args = {
-  state: 'negative',
-  icon: 'error',
-  filledIcon: true,
+export const Positive = {
+  render: TemplateRender,
+  args: { state: 'positive', icon: 'check' },
 };
 
-export const Caution = Template.bind({});
-Caution.args = {
-  state: 'caution',
-  icon: 'warning',
-  filledIcon: true,
+export const Negative = {
+  render: TemplateRender,
+  args: { state: 'negative', icon: 'error', filledIcon: true },
 };
 
-export const Loading = LoadingTemplate.bind({});
-Loading.args = {
-  state: 'primary',
+export const Caution = {
+  render: TemplateRender,
+  args: { state: 'caution', icon: 'warning', filledIcon: true },
+};
+
+export const Loading = {
+  render: LoadingRender,
+  args: { state: 'primary' },
 };
